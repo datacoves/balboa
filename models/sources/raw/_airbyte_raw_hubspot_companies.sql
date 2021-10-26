87e737884e3766c1fc3be51aab072ec7bccd16fd
Sebastian Sassi <ssassi@gmail.com>
2021-10-26 07:58:33 -0700

    Test CI.

--- models/sources/raw/_airbyte_raw_hubspot_companies.sql
+++ models/sources/raw/_airbyte_raw_hubspot_companies.sql
@@ -7,17 +7,18 @@ with raw_source as (
 final as (
 
     select
-        _airbyte_ab_id,
-        _airbyte_emitted_at,
-        _airbyte_data:archived::varchar as archived,
-        _airbyte_data:contacts::varchar as contacts,
-        _airbyte_data:createdat::varchar as createdat,
-        _airbyte_data:id::varchar as id,
-        _airbyte_data:properties::varchar as properties,
-        _airbyte_data:updatedat::varchar as updatedat
+        _airbyte_data:"archived"::varchar as archived,
+        _airbyte_data:"contacts"::varchar as contacts,
+        _airbyte_data:"createdAt"::varchar as createdat,
+        _airbyte_data:"id"::varchar as id,
+        _airbyte_data:"properties"::varchar as properties,
+        _airbyte_data:"updatedAt"::varchar as updatedat,
+        "_AIRBYTE_AB_ID" as _airbyte_ab_id,
+        "_AIRBYTE_EMITTED_AT" as _airbyte_emitted_at
 
     from raw_source
 
 )
 
 select * from final
+
