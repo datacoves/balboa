15346fb8a6d22fa1623af9823a1a779604fea397
Sebastian Sassi <ssassi@gmail.com>
2021-10-26 08:38:49 -0700

    Fix linting errors.

--- models/sources/raw/_airbyte_raw_hubspot_companies.sql
+++ models/sources/raw/_airbyte_raw_hubspot_companies.sql
@@ -1,20 +1,22 @@
 with raw_source as (
 
-    select * from {{ source('raw', '_airbyte_raw_hubspot_companies') }}
+    select
+        *
+    from {{ source('raw', '_airbyte_raw_hubspot_companies') }}
 
 ),
 
 final as (
 
     select
+        "_AIRBYTE_AB_ID" as _airbyte_ab_id,
+        "_AIRBYTE_EMITTED_AT" as _airbyte_emitted_at,
         _airbyte_data:"archived"::varchar as archived,
         _airbyte_data:"contacts"::varchar as contacts,
         _airbyte_data:"createdAt"::varchar as createdat,
         _airbyte_data:"id"::varchar as id,
         _airbyte_data:"properties"::varchar as properties,
-        _airbyte_data:"updatedAt"::varchar as updatedat,
-        "_AIRBYTE_AB_ID" as _airbyte_ab_id,
-        "_AIRBYTE_EMITTED_AT" as _airbyte_emitted_at
+        _airbyte_data:"updatedAt"::varchar as updatedat
 
     from raw_source
 
