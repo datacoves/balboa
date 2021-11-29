2fb945417e6e26229a2d86acf0cc49379dc9af24
Chris Jenkins <chris@convexa.ai>
2021-11-29 09:20:03 -0800

    Updated countries with new sqlfluff rules

--- models/country_analysis/countries.sql
+++ models/country_analysis/countries.sql
@@ -5,5 +5,5 @@ select
     current_population.population
 from {{ ref('base_country_codes') }} as countries
 left join {{ ref('current_population') }} as current_population
-  on
-    current_population.country_code = countries.iso3166_1_alpha_3
+    on
+        current_population.country_code = countries.iso3166_1_alpha_3
