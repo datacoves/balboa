35ac87bbb47ee8ce3eae60ccddfefc09bc4aec51
Chris Jenkins <chris@convexa.ai>
2021-12-07 09:22:42 -0800

    testing access to env

--- automate/dbt/get_artifacts.sh
+++ automate/dbt/get_artifacts.sh
@@ -4,5 +4,6 @@
 set -e
 
 cd $DBT_HOME
+echo $DBT_HOME
 dbt run-operation get_last_manifest --args '{"artifacts_location": "balboa.dbt_artifacts.artifacts"}' | awk '/{/ { f = 1 } f' > logs/manifest.json
 echo "Updated manifest from production"
