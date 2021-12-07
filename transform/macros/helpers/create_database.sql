98c04503100d1d5332ecd9191538bf2310f26aba
Chris Jenkins <chris@convexa.ai>
2021-12-07 11:21:56 -0800

    Added check if db exists in macro

--- transform/macros/helpers/create_database.sql
+++ transform/macros/helpers/create_database.sql
@@ -5,12 +5,20 @@
 #}
 
 {%- macro create_database() -%}
-  {% set create_db_sql %}
-      use role z_create_database;
-      create database if not exists {{ target.database }};
-      grant ownership on database {{ target.database }} to role {{ target.role }};
-      use role {{ target.role }};
-  {% endset %}
-  {% do run_query(create_db_sql) %}
-  {{ log("Created Database: " ~ target.database, info=true) }}
+  {%- set database_exists = adapter.get_relation(
+      database=target.database,
+      schema="information_schema",
+      identifier="tables") -%}
+  {% if not database_exists %}
+    {% set create_db_sql %}
+        use role z_create_database;
+        create database {{ target.database }};
+        grant ownership on database {{ target.database }} to role {{ target.role }};
+        use role {{ target.role }};
+    {% endset %}
+    {% do run_query(create_db_sql) %}
+    {{ log("Created Database: " ~ target.database, info=true) }}
+  {% else %}
+    {{ log("Database already exists: " ~ target.database, info=true) }}
+  {% endif %}
 {%- endmacro -%}
