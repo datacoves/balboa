0bb2af55f682ff37aef483e9c6ad7c5db862b907
Chris Jenkins <chris@convexa.ai>
2021-12-07 09:54:54 -0800

    Added pr database creation to PR CI

---
+++ transform/macros/helpers/create_database.sql
@@ -0,0 +1,14 @@
+{# This macro creates a database for use in pull request CI scripts. #}
+{#
+    To run: 
+    dbt run-operation create_database --args '{target_db: demo_db2}'
+#}
+
+{%- macro create_database(target_db) -%}
+  {% set create_db_sql %}
+      DROP DATABASE IF EXISTS {{ target_db }};
+      CREATE DATABASE IF NOT EXISTS {{ target_db }};
+  {% endset %}
+  {% do run_query(create_db_sql) %}
+  {{ log("Created Database: " ~ target_db, info=true) }}
+{%- endmacro -%}
