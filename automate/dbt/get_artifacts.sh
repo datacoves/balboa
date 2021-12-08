de06978ff6397449a73b436c2e26a4f53f69a03e
Chris Jenkins <chris.jenkins@trustpower.co.nz>
2021-12-09 05:13:10 +1300

    Revert "Revert "Merge pull request #8 from datacoves/ci-processes""
    This reverts commit 761299f211476daaf130e2b03c44901a6ca21758.

---
+++ automate/dbt/get_artifacts.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Cause script to exit on error
+set -e
+
+cd $DBT_HOME
+dbt run-operation get_last_manifest --args '{"artifacts_location": "balboa.dbt_artifacts.artifacts"}' | awk '/{/ { f = 1 } f' > logs/manifest.json
+echo "Updated manifest from production"
