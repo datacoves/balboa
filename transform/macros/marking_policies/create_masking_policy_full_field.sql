41e4b8480dfb8ee20db10186bab38992223db4bb
Noel Gomez <gomezn@convexa.ai>
2021-12-08 22:39:32 -0800

    add permifrost files

--- transform/macros/marking_policies/create_masking_policy_full_field.sql
+++ transform/macros/marking_policies/create_masking_policy_full_field.sql
@@ -1,9 +1,10 @@
 {% macro create_masking_policy_full_field(node_database,node_schema) %}
 
-create or replace masking policy {{node_database}}.{{node_schema}}.full_field as (val string) 
-  returns string ->
+CREATE MASKING POLICY IF NOT EXISTS {{node_database}}.{{node_schema}}.full_field AS (val string) 
+  RETURNS string ->
       CASE WHEN CURRENT_ROLE() IN ('ANALYST') THEN val 
            WHEN CURRENT_ROLE() IN ('SYSADMIN') THEN SHA2(val)
       ELSE '**********'
       END
+
 {% endmacro %}
