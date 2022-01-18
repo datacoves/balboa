7fda749c2f43bc40ee77c2164477319e71e0e1f0
Chris Jenkins <chris@convexa.ai>
2022-01-18 11:31:22 -0800

    Changed airflow logging

--- automate/blue_green_run.py
+++ automate/blue_green_run.py
@@ -4,7 +4,7 @@ import sys
 import os
 import time
 import logging
-
+LOGGER = LOGGER.getLogger("airflow.task")
 
 DBT_PROJECT_DIR = "/opt/airflow/dags/repo/balboa.git/transform"
 DBT_PROD_DB_NAME = "balboa"
@@ -27,51 +27,51 @@ def main(args):
     commit_hash = get_commit_hash()
     cwd = f"/home/airflow/transform-pr-{commit_hash}"
 
-    logging.info("Copying dbt project to temp directory")
+    LOGGER.info("Copying dbt project to temp directory")
     subprocess.run(["cp", "-rf", DBT_PROJECT_DIR, cwd], check=True)
 
-    logging.info("Loading dbt dependencies")
+    LOGGER.info("Loading dbt dependencies")
     subprocess.run(["dbt", "deps"], check=True, cwd=cwd)
 
-    logging.info("Setting db to staging database")
+    LOGGER.info("Setting db to staging database")
     os.environ['DBT_DATABASE'] = DBT_STAGING_DB_NAME
     #subprocess.run(["export", "DBT_DATABASE="+DBT_STAGING_DB_NAME], check=True, cwd=cwd)
     
-    logging.info("Checking that staging database does not exist")
+    LOGGER.info("Checking that staging database does not exist")
     while 1:
         try:
             subprocess.run(["dbt", "--no-write-json", "run-operation", "check_db_does_not_exist", "--args", '{"db_name": "%d"}' % (DBT_STAGING_DB_NAME)], check=True, cwd=cwd)
             break
         except:
-            logging.info("Staging database "+DBT_STAGING_DB_NAME+" exists, waiting 60 seconds to try again")
+            LOGGER.info("Staging database "+DBT_STAGING_DB_NAME+" exists, waiting 60 seconds to try again")
             time.sleep(60)
 
-    logging.info("Cloning db to 'staging'")
+    LOGGER.info("Cloning db to 'staging'")
     subprocess.run(["dbt", "run-operation", "clone_database", "--args", "'{source_db: "+DBT_PROD_DB_NAME+", target_db: "+DBT_STAGING_DB_NAME+"}'"], check=True, cwd=cwd)
 
     if tag: #If specific run is requested
-        logging.info("Running dbt build "+tag+"+")
+        LOGGER.info("Running dbt build "+tag+"+")
         subprocess.run(["dbt", "build", "-s", f"tag:{tag}+"], check=True, cwd=cwd)
     else:
-        logging.info("Getting prod manifest")
+        LOGGER.info("Getting prod manifest")
         subprocess.run(["export", "DBT_HOME="+cwd], check=True, cwd=cwd) #this env_var is referenced by get_artifacts
         subprocess.run(["/opt/airflow/dags/repo/balboa.git/automate/dbt/get_artifacts.sh"], check=True, cwd=cwd)
-        logging.info("Building all changes and their dependencies")
+        LOGGER.info("Building all changes and their dependencies")
         subprocess.run(["dbt", "build", '--state', 'logs', '-s', 'state:modified+'], check=True, cwd=cwd)
 
-    logging.info("Swapping staging database "+DBT_STAGING_DB_NAME+" with production "+DBT_PROD_DB_NAME)
+    LOGGER.info("Swapping staging database "+DBT_STAGING_DB_NAME+" with production "+DBT_PROD_DB_NAME)
     subprocess.run(["dbt", "run-operation", "swap_database", "--args", "'{db1: "+DBT_PROD_DB_NAME+", db2: "+DBT_STAGING_DB_NAME+"}'"], check=True, cwd=cwd)
     
-    logging.info("Uploading new prod manifest")
+    LOGGER.info("Uploading new prod manifest")
     subprocess.run(["dbt", "--no-write-json", "run-operation", "upload_manifest_catalog"], check=True, cwd=cwd)
 
-    logging.info("Removing dbt project temp directory")
+    LOGGER.info("Removing dbt project temp directory")
     subprocess.run(["rm", "-rf", cwd], check=True)
 
 if __name__ == "__main__":
     try:
         main(sys.argv[1:])
     except Exception as ex:
-        logging.info(ex)
+        LOGGER.info(ex)
         exit(1)
 
