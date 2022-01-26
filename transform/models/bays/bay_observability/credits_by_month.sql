a8ed7622ed877226999e4fd70ed361cab66c6cdc
Noel Gomez <noel_gomez@yahoo.com>
2022-01-26 12:05:05 -0800

    Usage (#27)
    * Added observability sources, created observability bay folder and files
    
    * Fixed sqlfluff issues
    
    * added views to create dashboards
    
    * Fixed mistakes, linting
    
    * Added yml files, fixed descriptions
    
    * fix linting issues
    
    Co-authored-by: Emiliano Fripp <emiliano@convexa.ai>
    Co-authored-by: Noel Gomez <gomezn@convexa.ai>

---
+++ transform/models/bays/bay_observability/credits_by_month.sql
@@ -0,0 +1,17 @@
+with cte as (
+    select
+        date_trunc(month, start_time) as month_n,
+        sum(credits_used) as monthly_credits
+    from {{ ref('stg_warehouse_metering_history') }}
+    where
+        start_time >= dateadd(year, -1, date_trunc(month, current_date)) and start_time < date_trunc(month, current_date)
+    group by month_n
+    order by month_n asc
+)
+
+select
+    month_n,
+    sum(monthly_credits) over(order by month_n asc rows between unbounded preceding and current row) as cumulative_sum
+from
+    cte
+order by month_n asc
