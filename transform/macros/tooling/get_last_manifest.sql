bb2d181f1f734dc1fd157e3b724203845c16506a
Noel Gomez <gomezn@convexa.ai>
2022-02-03 05:04:07 -0800

    updates schemas, roles, and manifest extraction

--- transform/macros/tooling/get_last_manifest.sql
+++ transform/macros/tooling/get_last_manifest.sql
@@ -8,6 +8,7 @@
 {% macro get_last_manifest(artifacts_location = 'balboa.dbt_artifacts.dbt_artifacts') %}
 
     {% set results = run_query("select data from " ~ artifacts_location ~ " where artifact_type = 'manifest.json' order by generated_at desc limit 1") %}
-    {{ log(results.columns[0].values()[0], info=True) }}
+    {# This newline is here so we can excluse any dbt related warning lines and timestamps when bringing down the manifest from Snowflake #}
+    {{ log("\n" + results.columns[0].values()[0], info=True) }}
 
 {% endmacro %}
