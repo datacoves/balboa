0ca32d1f533f2385a529fe1492d8b7fde431510f
Noel Gomez <noel_gomez@yahoo.com>
2022-02-04 15:11:10 -0800

    Update roles (#30)
    * resolve merge conflicts
    
    * ignore dbt_packages dir
    
    * updated permissions
    
    * updates schemas, roles, and manifest extraction
    
    * update macro to use role transformer_dbt_prd
    
    * updates to scripts, roles, and dbt package
    
    * Patched issue with dbt_artifacts
    
    * Updated dbt_artifacts version
    
    * Updated with 0.7 run_results
    
    * add datameer user
    
    Co-authored-by: Noel Gomez <gomezn@convexa.ai>
    Co-authored-by: Chris Jenkins <chris@convexa.ai>

--- secure/run_permifrost.sh
+++ secure/run_permifrost.sh
@@ -2,27 +2,42 @@
 
 #NOTE: Install the svc_permifrost_key.p8 key to ~/.ssh
 
-export PERMISSION_BOT_USER=SVC_PERMIFROST
-export PERMISSION_BOT_ACCOUNT=PX18100.EU-WEST-1
-export PERMISSION_BOT_WAREHOUSE=WH_TRANSFORMATION
-export PERMISSION_BOT_DATABASE=DEV_RAW
+export DATACOVES_USER=gomezn
+export PERMISSION_BOT_ACCOUNT=toa80779
+export PERMISSION_BOT_WAREHOUSE=WH_TRANSFORMING
+export PERMISSION_BOT_DATABASE=RAW
 export PERMISSION_BOT_ROLE=SECURITYADMIN
-export PERMISSION_BOT_KEY_PATH="/config/.ssh/svc_permifrost_key.p8"
+export PERMISSION_BOT_USER="$DATACOVES_USER"
+export PERMISSION_BOT_KEY_PATH="/config/.ssh/user_permifrost.p8"
 export PERMISSION_BOT_KEY_PASSPHRASE=permifrost
 export PATH=$PATH:/config/.local/bin
 
-# This requires installation of our Permifrost fork: python -m pip install git+https://gitlab.com/datacoves/permifrost.git
-# python -m pip install git+https://gitlab.com/datacoves/permifrost.git
+# This requires installation of our Permifrost fork:
+# python -m pip install git+https://gitlab.com/datacoves-oss/permifrost
+
+# GENERATE A KEY AND ADD TO SNWOFLAKE
+# Run this in the terminal
+# 
+# openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out /config/.ssh/user_permifrost.p8
+# Use passphrase = permifrost
+# openssl rsa -in /config/.ssh/user_permifrost.p8 -pubout -out /config/.ssh/user_permifrost.pub
+# cat ~/.ssh/user_permifrost.pub
+#
+# Run this in Snowflake
+# use role securityadmin;
+# alter user <Snowflake Username all caps> set rsa_public_key='<Key from above>';
+
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 echo "Current Branch is:" $BRANCH
 
 # Only apply role changes from main branch
-if [ $BRANCH != 'main' ]
+if [ $BRANCH == 'main' ]
 then
     echo 'This script can only be run from the main branch';
     exit 1;
 else
     # This allows for running permifrost from any dir and returns to that dir at end of executiuon
-    cd /config/workspace/secure/ && permifrost run permifrost.yml ; cd -
+    cd /config/workspace/secure/ && permifrost run --diff permifrost.yml ; cd -
+    #run permifrost.yml ; cd -
 fi
