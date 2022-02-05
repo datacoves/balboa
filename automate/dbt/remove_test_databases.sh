aac0ddaad38009bfe403a9ad9d1353807aec19c5
Noel Gomez <gomezn@convexa.ai>
2022-02-04 16:00:58 -0800

    fix removal of test dbs

--- automate/dbt/remove_test_databases.sh
+++ automate/dbt/remove_test_databases.sh
@@ -6,4 +6,4 @@ export merged_prs=$(git log --all --oneline | grep -m 20 -Eo "Merge pull request
 # sort: Cut to just unique rows, and sort by number in reverse (2 goes before 10, would be after if sorted alphabetically)
 # tr, sed: Change newlines to pipes, to make a single string - and remove the last pipe
 
-dbt run-operation remove_closed_prs --args "{pr_ids: $merged_prs}"
+dbt run-operation remove_closed_pr_dbs --args "{pr_ids: $merged_prs}"
