9f2eb90af223f37f942c0c11aa15f1f8c6ecfde0
Bruno Antonellini <antonellinibruno@gmail.com>
2022-02-18 15:15:08 -0300

    removed conflicting sources snapshots and models

--- transform/models/bays/bay_observability/credits_total.sql
+++
@@ -1,5 +0,0 @@
-select sum(credits_used) as credits_used
-from
-    {{ ref('stg_warehouse_metering_history') }}
-where
-    timestampdiff(month, start_time, current_date) <= 12 and timestampdiff(month, start_time, current_date) > 0
