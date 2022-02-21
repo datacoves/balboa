563fae55a25106d96483c7e7f6a904341263e904
Noel Gomez <gomezn@convexa.ai>
2022-02-21 14:39:34 -0800

    create stage after cloning

---
+++ transform/macros/tooling/create_dbt_artifacts_stage.sql
@@ -0,0 +1,16 @@
+{# This macro creates an internal stage for dbt_artifacts #}
+{#
+    To run: 
+    dbt run-operation create_dbt_artifacts_stage
+#}
+
+{%- macro create_dbt_artifacts_stage(db_nmae) -%}
+  {% set sql %}
+      create stage if not exists {{ target.database }}.source_dbt_artifacts.artifacts
+      file_format = ( type =  json );
+  {% endset %}
+  {% do run_query(sql) %}
+  {{ log("Created DBT Artifacts Stage in database: " + target.database, info=true) }}
+{%- endmacro -%}
+
+
