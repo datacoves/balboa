9b310b55d73d328150dd37a88dde3d6e92053cb2
jc628 <jchadwick628@gmail.com>
2022-02-23 16:03:03 -0800

    add feature to master metric table for YTD, MTD aggregations

--- transform/models/metric_sample/master_metrics/mkt_master_metric.sql
+++ transform/models/metric_sample/master_metrics/mkt_master_metric.sql
@@ -14,8 +14,35 @@
     database='balboa_dev'
 ) %}
 
+with 
+unioned_data as (
+    {{ dbt_utils.union_relations(
+        relations=metric_models,
+        include=dimension_list,
+        column_override= {"CAMPAIGN_EXECUTION_DATETIME":"DATE"}
+    ) }}
+),
 
-{{ dbt_utils.union_relations(
-    relations=metric_models,
-    include=dimension_list 
-) }}
+-- this part is a little sloppy. it includes some additional requirements that negate some flexibility
+-- written like this, campaign_execution_datetime is a required field in the dimensions_list
+-- additionally, metric_value has to be the final field in the dimension_list or else the for loop won't work right
+-- the most robust solution would be to take the two dbt_utils macros used above and rework them exactly for our purposes into one model
+final as (
+    select 
+        {% for dim in dimension_list %}
+        {% if not loop.last%} {{dim}}, {% endif %}
+        {% endfor %}
+        current_date() as reporting_as_of,
+        coalesce (sum (case when campaign_execution_datetime :: date = current_date() then metric_value end), 0) as metric_current_date, 
+        coalesce (sum (case when date_trunc(week, campaign_execution_datetime) = date_trunc(week, current_date()) then metric_value end), 0) as metric_week_to_date,
+        coalesce (sum (case when date_trunc(month, campaign_execution_datetime) = date_trunc(month, current_date()) then metric_value end), 0) as metric_month_to_date,
+        coalesce (sum (case when date_trunc(quarter, campaign_execution_datetime) = date_trunc(quarter, current_date()) then metric_value end), 0) as metric_quarter_to_date,
+        coalesce (sum (case when date_trunc(year, campaign_execution_datetime) = date_trunc(year, current_date()) then metric_value end), 0) as metric_year_to_date
+    from unioned_data
+    group by 
+        {% for dim in dimension_list %}
+        {{dim}} {% if not loop.last%} , {% endif %}
+        {% endfor %}
+)
+
+select * from final
