4822032571f4e73e6ec10f4ebd024eebd349a1d2
Sebastian Sassi <ssassi@gmail.com>
2022-02-26 10:12:08 -0800

    Fix dags loader script.

--- automate/airflow/dags/dags_loader.py
+++ automate/airflow/dags/dags_loader.py
@@ -3,9 +3,35 @@ import dagfactory
 import glob
 import pickle
 import subprocess
-from datetime import datetime
+from datetime import datetime, timezone
 from airflow import DAG
 from pathlib import Path
+
+
+WORKING_DIR = Path("/home/airflow")
+TIMEOUT = 300   # seconds
+
+
+def is_running(running_flag):
+    """Checks running flag file and return if process is still running"""
+    if running_flag.exists():
+        mtime = datetime.fromtimestamp(running_flag.stat().st_mtime, tz=timezone.utc)
+        expired = (datetime.utcnow() - mtime).seconds > TIMEOUT
+        if expired:
+            running_flag.unlink()
+        return not expired
+    return False
+
+
+def register_dags(all_dags):
+    """Register all dags in globals for airflow to load them"""
+    dagfactory.DagFactory.register_dags(all_dags, globals())
+    dags_wo_default = [x for x in all_dags.keys() if x != 'default']
+    if dags_wo_default:
+        random_dag = dags_wo_default[0]
+        if random_dag in globals() and globals()[random_dag].tasks:
+            # Evaluating one random task so Airflow updates dags definition
+            globals()[random_dag].tasks[0]
 
 
 def main():
@@ -16,28 +42,32 @@ def main():
         cwd=dags_folder).stdout.strip("\n")
     print(f"Generating dags for commit '{current_commit}'")
     
-    current_pickle = f"/home/airflow/{current_commit}.pickle"
-    if Path(current_pickle).exists():
-        with open(current_pickle, "rb") as f:
-            all_dags = pickle.load(f)
+    running_flag = WORKING_DIR / "running"
+    if is_running(running_flag):
+        return
     else:
-        yaml_config_files = glob.glob(f"{dags_folder}/*.yml") + glob.glob(f"{dags_folder}/*.yaml")
+        running_flag.touch()
+    
+    try:
+        current_pickle = WORKING_DIR / f"{current_commit}.pickle"
+        if current_pickle.exists():
+            # Load cached dags
+            with open(current_pickle, "rb") as f:
+                all_dags = pickle.load(f)
+        else:
+            # Recalculate dags
+            yaml_config_files = glob.glob(f"{dags_folder}/*.yml") + glob.glob(f"{dags_folder}/*.yaml")
 
-        all_dags = dict()
-        for config_file in yaml_config_files:
-            dag_factory = dagfactory.DagFactory(os.path.abspath(config_file))
-            dags = dag_factory.build_dags()
-            all_dags.update(dags)
-        with open(current_pickle, "wb") as f:
-            pickle.dump(all_dags, f)
+            all_dags = dict()
+            for config_file in yaml_config_files:
+                dag_factory = dagfactory.DagFactory(os.path.abspath(config_file))
+                dags = dag_factory.build_dags()
+                all_dags.update(dags)
+            with open(current_pickle, "wb") as f:
+                pickle.dump(all_dags, f)
 
-    dagfactory.DagFactory.register_dags(all_dags, globals())
-    dags_wo_default = [x for x in all_dags.keys() if x != 'default']
-    if dags_wo_default:
-        random_dag = dags_wo_default[0]
-        if random_dag in globals() and globals()[random_dag].tasks:
-            # Evaluating one random task so Airflow updates dags definition
-            globals()[random_dag].tasks[0]
-
+        register_dags(all_dags)
+    finally:
+        running_flag.unlink()
 
 main()
