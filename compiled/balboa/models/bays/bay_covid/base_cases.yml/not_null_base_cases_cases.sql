5ae177220d1469f4e6167fb80e33a7b3a855d760
ssassi <ssassi@sophilabs.com>
2022-05-23 12:48:05 +0000

    Deploying to dbt-docs from @ datacoves/balboa@ca072836c625729e5f706aca1d8831973d2c97ff 🚀

--- compiled/balboa/models/bays/bay_covid/base_cases.yml/not_null_base_cases_cases.sql
+++ compiled/balboa/models/bays/bay_covid/base_cases.yml/not_null_base_cases_cases.sql
@@ -2,6 +2,8 @@
     
     
 
+
+
 select *
 from BALBOA.bay_covid.base_cases
 where cases is null
--- compiled/balboa/models/sources/nyt_covid_cases/nyt_covid_cases.yml/source_not_null_nyt_covid_nyt_covid_data__airbyte_ab_id.sql
+++ compiled/balboa/models/sources/nyt_covid_cases/nyt_covid_cases.yml/source_not_null_nyt_covid_nyt_covid_data__airbyte_ab_id.sql
@@ -2,6 +2,8 @@
     
     
 
+
+
 select *
 from raw.raw._AIRBYTE_RAW_NYT_COVID_DATA
 where _airbyte_ab_id is null
