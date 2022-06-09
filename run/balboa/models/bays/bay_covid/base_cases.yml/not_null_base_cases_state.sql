0f9c8d95dab272f17cbbe441e21ea24f32a4cc9d
noel <noel_gomez@yahoo.com>
2022-06-09 13:19:55 +0000

    Deploying to dbt-docs from @ datacoves/balboa@4406d54b57d9cf301e1d0dbdbe28ffab964a0da3 🚀

---
+++ run/balboa/models/bays/bay_covid/base_cases.yml/not_null_base_cases_state.sql
@@ -0,0 +1,11 @@
+select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      
+        select *
+        from staging_BALBOA.dbt_test__audit.not_null_base_cases_state
+    
+      
+    ) dbt_internal_test
---
+++ run/balboa/models/sources/country_data/_airbyte_raw_country_populations.sql
@@ -0,0 +1,66 @@
+
+  create or replace  view staging_BALBOA.source_country_data._airbyte_raw_country_populations
+  
+    
+    
+(
+  
+    
+      COUNTRY_CODE
+    
+    , 
+  
+    
+      COUNTRY_NAME
+    
+    , 
+  
+    
+      VALUE
+    
+    , 
+  
+    
+      YEAR
+    
+    , 
+  
+    
+      _AIRBYTE_AB_ID
+    
+    , 
+  
+    
+      _AIRBYTE_EMITTED_AT
+    
+    
+  
+)
+
+  copy grants as (
+    with raw_source as (
+
+    select
+        parse_json(replace(_airbyte_data::string, '"NaN"', 'null')) as airbyte_data_clean,
+        *
+    from raw.raw._AIRBYTE_RAW_COUNTRY_POPULATIONS
+
+),
+
+final as (
+
+    select
+        airbyte_data_clean:"Country Code"::varchar as country_code,
+        airbyte_data_clean:"Country Name"::varchar as country_name,
+        airbyte_data_clean:"Value"::varchar as value,
+        airbyte_data_clean:"Year"::varchar as year,
+        "_AIRBYTE_AB_ID" as _airbyte_ab_id,
+        "_AIRBYTE_EMITTED_AT" as _airbyte_emitted_at
+
+    from raw_source
+
+)
+
+select * from final
+order by 1
+  );
