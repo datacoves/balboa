4f47110f68bc977cc753487aef9feefd16764f75
Noel Gomez <gomezn@convexa.ai>
2022-06-30 08:13:46 -0700

    force true on finding manifest

--- automate/dbt/get_artifacts.sh
+++ automate/dbt/get_artifacts.sh
@@ -12,7 +12,8 @@ if [ $LINES_IN_MANIFEST -eq 0 ]
 then
     echo "Manifest for this version of dbt not found in Snowflake, contact the Snowflake administrator to load a updated manifest to snowflake."
     echo "::set-output name=manifest_found::false"
-    export MANIFEST_FOUND=0
+    # export MANIFEST_FOUND=0
+    export MANIFEST_FOUND=1
 else
     echo "Updated manifest from production"
     echo "::set-output name=found::true"
