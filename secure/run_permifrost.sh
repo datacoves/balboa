052622c45e86bda0dbacd369ff159c7e739c16dc
Bruno Antonellini <antonellinibruno@gmail.com>
2022-07-06 09:39:21 -0300

    New run_permifrost.sh

--- secure/run_permifrost.sh
+++ secure/run_permifrost.sh
@@ -1,42 +1,30 @@
 #! /bin/dash
 
-#NOTE: Install the svc_permifrost_key.p8 key to ~/.ssh
+# NOTE: you must generate an encrypted version of your ssh key before running permifrost
+# you will need to rerun this when the key is rotated
+# openssl rsa -des -in ~/.ssh/id_rsa -out ~/.ssh/id_rsa_encrypted.key
 
 export PERMISSION_BOT_ACCOUNT=toa80779
 export PERMISSION_BOT_WAREHOUSE=WH_TRANSFORMING
-export PERMISSION_BOT_DATABASE=RAW
+export PERMISSION_BOT_DATABASE=BALBOA_DEV
 export PERMISSION_BOT_ROLE=SECURITYADMIN
 export PERMISSION_BOT_USER="${DATACOVES_USER_EMAIL%@*}"
-export PERMISSION_BOT_KEY_PATH="/config/.ssh/user_permifrost.p8"
+export PERMISSION_BOT_KEY_PATH="/config/.ssh/id_rsa_encrypted.key"
 export PERMISSION_BOT_KEY_PASSPHRASE=permifrost
 export PATH=$PATH:/config/.local/bin
 
-# This requires installation of our Permifrost fork:
-# python -m pip install git+https://gitlab.com/datacoves-oss/permifrost
-
-# GENERATE A KEY AND ADD TO SNWOFLAKE
-# Run this in the terminal
-# 
-# openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out /config/.ssh/user_permifrost.p8
-# Use passphrase = permifrost
-# openssl rsa -in /config/.ssh/user_permifrost.p8 -pubout -out /config/.ssh/user_permifrost.pub
-# cat ~/.ssh/user_permifrost.pub
-#
-# Run this in Snowflake
-# use role securityadmin;
-# alter user <Snowflake Username all caps> set rsa_public_key='<Key from above>';
-
+# This requires installation of our Permifrost fork: python -m pip install git+https://gitlab.com/datacoves/permifrost.git
+# python -m pip install git+https://gitlab.com/datacoves/permifrost.git
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 echo "Current Branch is:" $BRANCH
 
 # Only apply role changes from main branch
-if [ $BRANCH == 'main' ]
-then
-    echo 'This script can only be run from the main branch';
-    exit 1;
+if [ $BRANCH != 'main' ]; then
+    echo 'This script can only be run from the main branch'
+    exit 1
 else
     # This allows for running permifrost from any dir and returns to that dir at end of executiuon
-    cd /config/workspace/secure/ && permifrost run --diff permifrost.yml ; cd -
-    #run permifrost.yml ; cd -
+    cd /config/workspace/secure/ && permifrost run permifrost.yml
+    cd -
 fi
