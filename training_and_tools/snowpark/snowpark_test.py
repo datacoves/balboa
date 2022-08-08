f84fab4803a02cc694d002dc52113307be698b59
Noel Gomez <gomezn@convexa.ai>
2022-08-08 15:16:21 -0700

    add snowpark example and cleanup old files

---
+++ training_and_tools/snowpark/snowpark_test.py
@@ -0,0 +1,66 @@
+from snowflake.snowpark import Session
+import yaml
+
+from pathlib import Path
+home = str(Path.home())
+DBT_PROFILES_YML = Path(home) / ".dbt" / "profiles.yml"
+
+# NOTE: pyarrow needs to be this version for snowpark
+# pyarrow==8.0.0
+
+# Needed to create the snowflake session using key based auth
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives import serialization
+
+def generate_connection_properties(profile = 'default', target='dev'):
+    with open(DBT_PROFILES_YML, "r") as stream:
+        try:
+            profiles = yaml.safe_load(stream)
+            snowflake_conn_properties = profiles[profile]['outputs'][target]
+
+            if "private_key_path" in snowflake_conn_properties:
+                print('Using Key Based Authentication')
+                # Read the private key defined in profiles.yml and transform it to the format 
+                # Snowflake expects
+                private_key_path = snowflake_conn_properties['private_key_path']
+                with open(private_key_path, "rb") as key:
+                    p_key = serialization.load_pem_private_key(
+                        key.read(),
+                        password=None
+                    )
+
+                    pkb = p_key.private_bytes(
+                        encoding=serialization.Encoding.DER,
+                        format=serialization.PrivateFormat.PKCS8,
+                        encryption_algorithm=serialization.NoEncryption())
+
+                snowflake_conn_properties['private_key'] = pkb
+            else:
+                print('Using Username / Password based Authentication')
+
+            return snowflake_conn_properties
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+connection_properties = generate_connection_properties()
+
+# # build connection to Snowflake
+session = Session.builder.configs(connection_properties).create()
+
+# find local pydicom location with : pip show pydicom
+# session.add_import("/usr/local/lib/python3.8/site-packages")  
+# session.add_import("pydicom")
+
+# You can run SQL directly like:
+session.sql("select current_warehouse() wh, current_database() db, current_schema() schema, current_version() ver").show()
+
+# df = session.table("BALBOA.BAY_COUNTRY.CURRENT_POPULATION")
+# df.describe().show()
+
+# rows = df.select('COUNTRY_CODE').count()
+# print(rows)
+
+print('done')
