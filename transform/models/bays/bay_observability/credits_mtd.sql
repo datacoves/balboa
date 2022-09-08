f705e1d7893eadac3454a849a11d0c0c74487909
Noel Gomez <gomezn@convexa.ai>
2022-09-07 20:52:07 -0700

    fix model and remove sqlfluff version display in pr action

--- transform/models/bays/bay_observability/credits_mtd.sql
+++ transform/models/bays/bay_observability/credits_mtd.sql
@@ -5,11 +5,10 @@ select
         from
             {{ ref('stg_warehouse_metering_history') }}
         where
-            timestampdiff(month, start_time, current_date) = 1 and
+            timestampdiff(month, start_time, current_date) = 1
             and day(current_date) >= day(start_time)
     ) as previous_mtd_credits_used
 from
     {{ ref('stg_warehouse_metering_history') }}
--- left join prev_month on this_month.start_time = prev_month.pm_start_time
 where
     timestampdiff(month, start_time, current_date) = 0
