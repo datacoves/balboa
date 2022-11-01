b10970fd377ca52c3234dea318918c310cf33c1a
Noel Gomez <gomezn@convexa.ai>
2022-11-01 06:13:50 -0700

    add dbt-coves templates

---
+++ transform/.dbt_coves/templates/source_model.sql
@@ -0,0 +1,35 @@
+with raw_source as (
+
+    select *
+    from {% raw %}{{{% endraw %} source('{{ relation.schema.lower() }}', '{{ relation.name.lower() }}') {% raw %}}}{% endraw %}
+
+),
+
+final as (
+
+    select
+{%- if adapter_name == 'SnowflakeAdapter' or adapter_name == 'RedshiftAdapter' %}
+{%- for key, cols in nested.items() %}
+  {%- for col in cols %}
+        {{ key }}:{{ '"' + col + '"' }}::{{ cols[col]["type"] }} as {{ cols[col]["id"] }}{% if not loop.last or columns %},{% endif %}
+  {%- endfor %}
+{%- endfor %}
+{%- for col in columns %}
+        {{ '"' + col['name'] + '"' }}::{{ col["type"] }} as {{ col['id'] }}{% if not loop.last %},{% endif %}
+{%- endfor %}
+{%- elif adapter_name == 'BigQueryAdapter' %}
+{%- for key, cols in nested.items() %}
+  {%- for col in cols %}
+        cast({{ key }}.{{ col }} as {{ cols[col]["type"].replace("varchar", "string") }}) as {{ cols[col]["id"] }}{% if not loop.last or columns %},{% endif %}
+  {%- endfor %}
+{%- endfor %}
+{%- for col in columns %}
+        cast({{ col['name'] }} as {{ col["type"].replace("varchar", "string") }}) as {{ col['id'] }}
+{%- endfor %}
+{%- endif %}
+
+    from raw_source
+
+)
+
+select * from final
