6339090c62d1e4a4bafd2ad7f64d09c998de63ee
noel <noel_gomez@yahoo.com>
2022-11-05 17:08:48 +0000

    Deploying to dbt-docs from @ datacoves/balboa@4fc5f8130fda6b1fe5bfc43f6d978a34a4063b98 🚀

--- compiled/balboa/models/inlets/loans/_airbyte_raw_personal_loans.sql
+++ compiled/balboa/models/inlets/loans/_airbyte_raw_personal_loans.sql
@@ -1,7 +1,7 @@
 with raw_source as (
 
     select *
-    from RAW.datameer._airbyte_raw_personal_loans
+    from RAW.loans._airbyte_raw_personal_loans
 
 ),
 
@@ -60,9 +60,9 @@ final as (
         _airbyte_data:"URL"::varchar as url,
         _airbyte_data:"VERIFICATION_STATUS"::varchar as verification_status,
         _airbyte_data:"ZIP_CODE"::varchar as zip_code,
-        "_AIRBYTE_AB_ID" as airbyte_ab_id,
-        "_AIRBYTE_DATA" as airbyte_data,
-        "_AIRBYTE_EMITTED_AT" as airbyte_emitted_at
+        "_AIRBYTE_AB_ID"::VARCHAR as airbyte_ab_id,
+        "_AIRBYTE_DATA"::VARIANT as airbyte_data,
+        "_AIRBYTE_EMITTED_AT"::TIMESTAMP_TZ as airbyte_emitted_at
 
     from raw_source
 
--- compiled/balboa/models/inlets/loans/_airbyte_raw_zip_coordinates.sql
+++ compiled/balboa/models/inlets/loans/_airbyte_raw_zip_coordinates.sql
@@ -1,7 +1,7 @@
 with raw_source as (
 
     select *
-    from RAW.datameer._airbyte_raw_zip_coordinates
+    from RAW.loans._airbyte_raw_zip_coordinates
 
 ),
 
@@ -11,9 +11,9 @@ final as (
         _airbyte_data:"LAT"::varchar as lat,
         _airbyte_data:"LON"::varchar as lon,
         _airbyte_data:"ZIP"::varchar as zip,
-        "_AIRBYTE_AB_ID" as airbyte_ab_id,
-        "_AIRBYTE_DATA" as airbyte_data,
-        "_AIRBYTE_EMITTED_AT" as airbyte_emitted_at
+        "_AIRBYTE_AB_ID"::VARCHAR as airbyte_ab_id,
+        "_AIRBYTE_DATA"::VARIANT as airbyte_data,
+        "_AIRBYTE_EMITTED_AT"::TIMESTAMP_TZ as airbyte_emitted_at
 
     from raw_source
 
