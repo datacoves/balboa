20d12ba8d494fdd440b52b64229a688b8d1dcab5
noel <noel_gomez@yahoo.com>
2022-11-09 16:38:21 +0000

    Deploying to dbt-docs from @ datacoves/balboa@4b9c9276fc073f482783384ae111083acae559da 🚀

---
+++ run/balboa/models/L2_bays/covid_observations/base_cases.sql
@@ -0,0 +1,51 @@
+
+  create or replace  view BALBOA_STAGING.l2_covid_observations.base_cases
+  
+    
+    
+(
+  
+    "COUNTRY_REGION" COMMENT $$$$, 
+  
+    "PROVINCE_STATE" COMMENT $$$$, 
+  
+    "COUNTY" COMMENT $$$$, 
+  
+    "FIPS" COMMENT $$FIPS Code$$, 
+  
+    "DATE" COMMENT $$Reporting date$$, 
+  
+    "CASE_TYPE" COMMENT $$$$, 
+  
+    "CASES" COMMENT $$Reported Covid-19 cases$$, 
+  
+    "LONG" COMMENT $$$$, 
+  
+    "LAT" COMMENT $$$$, 
+  
+    "ISO3166_1" COMMENT $$$$, 
+  
+    "ISO3166_2" COMMENT $$$$, 
+  
+    "DIFFERENCE" COMMENT $$$$, 
+  
+    "LAST_UPDATED_DATE" COMMENT $$$$, 
+  
+    "LAST_REPORTED_FLAG" COMMENT $$$$, 
+  
+    "NEW_CASES" COMMENT $$$$
+  
+)
+
+  copy grants as (
+    with final as (
+
+    select
+        *,
+        difference as new_cases
+    from starschema_covid19.public.JHU_COVID_19
+
+)
+
+select * from final
+  );
