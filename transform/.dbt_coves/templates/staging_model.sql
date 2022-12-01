6593d8a53db9f96be8004c4f18b6b4060209d292
Bruno Antonellini <antonellinibruno@gmail.com>
2022-12-01 10:11:44 -0300

    Updating templates

--- transform/.dbt_coves/templates/staging_model.sql
+++ transform/.dbt_coves/templates/staging_model.sql
@@ -3,7 +3,7 @@ with raw_source as (
     select
         parse_json(replace(_airbyte_data::string, '"NaN"', 'null')) as airbyte_data_clean,
         *
-    from {% raw %}{{{% endraw %} source('{{ relation.schema.lower() }}', '{{ relation.name.lower() }}') {% raw %}}}{% endraw %}
+    from {% raw %}{{{% endraw %} source('{{ relation.schema }}', '{{ relation.name }}') {% raw %}}}{% endraw %}
 
 ),
 
@@ -13,20 +13,20 @@ final as (
 {%- if adapter_name == 'SnowflakeAdapter' or adapter_name == 'RedshiftAdapter' %}
 {%- for key, cols in nested.items() %}
   {%- for col in cols %}
-        {{ key }}:{{ '"' + col + '"' }}::{{ cols[col]["type"] }} as {{ cols[col]["id"] }}{% if not loop.last or columns %},{% endif %}
+        {{ key }}:{{ '"' + col + '"' }}::{{ cols[col]["type"].lower() }} as {{ cols[col]["id"] }}{% if not loop.last or columns %},{% endif %}
   {%- endfor %}
 {%- endfor %}
 {%- for col in columns %}
-        {{ '"' + col['name'] + '"' }}::{{ col["type"] }} as {{ col['id'] }}{% if not loop.last %},{% endif %}
+        {{ '"' + col['name'] + '"' }}::{{ col["type"].lower() }} as {{ col['id'] }}{% if not loop.last %},{% endif %}
 {%- endfor %}
 {%- elif adapter_name == 'BigQueryAdapter' %}
 {%- for key, cols in nested.items() %}
   {%- for col in cols %}
-        cast({{ key }}.{{ col }} as {{ cols[col]["type"].replace("varchar", "string") }}) as {{ cols[col]["id"] }}{% if not loop.last or columns %},{% endif %}
+        cast({{ key }}.{{ col }} as {{ cols[col]["type"].lower().replace("varchar", "string") }}) as {{ cols[col]["id"] }}{% if not loop.last or columns %},{% endif %}
   {%- endfor %}
 {%- endfor %}
 {%- for col in columns %}
-        cast({{ col['name'] }} as {{ col["type"].replace("varchar", "string") }}) as {{ col['id'] }}
+        cast({{ col['name'] }} as {{ col["type"].lower().replace("varchar", "string") }}) as {{ col['id'] }}{% if not loop.last %},{% endif %}
 {%- endfor %}
 {%- endif %}
 
