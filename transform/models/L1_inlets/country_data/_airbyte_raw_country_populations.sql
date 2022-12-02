e81ad839a16450ed7deba5922e244c57d4056409
Noel Gomez <noel_gomez@yahoo.com>
2022-12-01 16:44:57 -0800

    update table and schema names to uppercase (#121)
    * update table and schema names to uppercase
    * add missing source description
    * update pre-commit config to point to datacoves repo
    * fix linting error
    
    Co-authored-by: Noel Gomez <gomezn@convexa.ai>

--- transform/models/L1_inlets/country_data/_airbyte_raw_country_populations.sql
+++ transform/models/L1_inlets/country_data/_airbyte_raw_country_populations.sql
@@ -3,19 +3,20 @@ with raw_source as (
     select
         parse_json(replace(_airbyte_data::string, '"NaN"', 'null')) as airbyte_data_clean,
         *
-    from {{ source('raw', '_airbyte_raw_country_populations') }}
+    from {{ source('RAW', '_AIRBYTE_RAW_COUNTRY_POPULATIONS') }}
 
 ),
 
 final as (
 
     select
-        airbyte_data_clean:"Country Code"::varchar as country_code,
-        airbyte_data_clean:"Country Name"::varchar as country_name,
-        airbyte_data_clean:"Value"::varchar as value,
-        airbyte_data_clean:"Year"::varchar as year,
-        "_AIRBYTE_AB_ID" as _airbyte_ab_id,
-        "_AIRBYTE_EMITTED_AT" as _airbyte_emitted_at
+        _airbyte_data:"Country Code"::varchar as country_code,
+        _airbyte_data:"Country Name"::varchar as country_name,
+        _airbyte_data:"Value"::varchar as value,
+        _airbyte_data:"Year"::varchar as year,
+        "_AIRBYTE_AB_ID"::varchar as airbyte_ab_id,
+        "_AIRBYTE_DATA"::variant as airbyte_data,
+        "_AIRBYTE_EMITTED_AT"::timestamp_tz as airbyte_emitted_at
 
     from raw_source
 
