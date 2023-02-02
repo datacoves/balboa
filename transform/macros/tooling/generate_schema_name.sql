a13d332bbb781c847c44272177671876a41a50be
Noel Gomez <noel_gomez@yahoo.com>
2023-02-02 14:46:04 -0800

    Update generate_schema_name.sql
    Update macro to account for snapshots

--- transform/macros/tooling/generate_schema_name.sql
+++ transform/macros/tooling/generate_schema_name.sql
@@ -1,16 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- elif target.name == 'dev' -%}
+    {%- set default_schema = node.config.target_schema or target.schema -%}
+    
+    {%- if custom_schema_name is none or target.name == 'dev' -%}
 
         {{ default_schema | trim }}
 
     {%- else -%}
-
+    
         {{ custom_schema_name | trim }}
 
     {%- endif -%}
