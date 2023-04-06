026fde24382641e0dba07beacdc65c185a9008b2
Noel Gomez <noel_gomez@yahoo.com>
2023-04-05 17:26:17 -0700

    Updates for sqlfluff compatibility (#143)
    * Updates for sqlfluff compatibility
    
    * add dbt project directory to sqlfluff config
    
    ---------
    
    Co-authored-by: Noel Gomez <gomezn@convexa.ai>

--- transform/models/L2_bays/covid_observations/covid_location.sql
+++ transform/models/L2_bays/covid_observations/covid_location.sql
@@ -3,8 +3,8 @@
 with jhu_covid_19 as (
     select distinct
         country_region,
-        COALESCE(province_state, 'UNDEFINED') as province_state,
-        COALESCE(county, 'UNDEFINED') as county,
+        coalesce(province_state, 'UNDEFINED') as province_state,
+        coalesce(county, 'UNDEFINED') as county,
         lat,
         long,
         iso3166_1,
@@ -15,8 +15,8 @@ with jhu_covid_19 as (
 
 rank_locations as (
     select
-        HASH(country_region || '|' || province_state || '|' || county) as snowflake_location_id,
-        {{ dbt_utils.generate_surrogate_key(['country_region', 'province_state', 'county']) }} as location_id, --noqa
+        hash(country_region || '|' || province_state || '|' || county) as snowflake_location_id,
+        {{ dbt_utils.generate_surrogate_key(['country_region', 'province_state', 'county']) }} as location_id,
         country_region as country,
         province_state as state,
         county,
@@ -24,7 +24,7 @@ rank_locations as (
         long,
         iso3166_1,
         iso3166_2,
-        RANK() over (partition by location_id order by date desc) as rowrank
+        rank() over (partition by location_id order by date desc) as rowrank
     from jhu_covid_19
 )
 
