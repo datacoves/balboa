e2fc291b746cbbf9cfe1c620d330a9f96c80aaf4
Noel Gomez <gomezn@convexa.ai>
2023-04-12 15:55:27 -0700

    Add David to snowflake users

--- secure/run_permifrost.sh
+++ secure/run_permifrost.sh
@@ -4,7 +4,7 @@ export PERMISSION_BOT_ACCOUNT=toa80779
 export PERMISSION_BOT_WAREHOUSE=WH_TRANSFORMING
 export PERMISSION_BOT_DATABASE=BALBOA_DEV
 export PERMISSION_BOT_ROLE=SECURITYADMIN
-export PERMISSION_BOT_USER="${DATACOVES_USER_EMAIL%@*}"
+export PERMISSION_BOT_USER="${DATACOVES__USER_EMAIL%@*}"
 export PERMISSION_BOT_KEY_PATH="/config/.ssl/prd-private.pem"
 export PATH=$PATH:/config/.local/bin
 
@@ -15,7 +15,7 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 echo "Current Branch is:" $BRANCH
 
 # Only apply role changes from main branch
-if [ $BRANCH == 'main' ]; then
+if [ $BRANCH != 'main' ]; then
     echo 'This script can only be run from the main branch'
     exit 1
 else
