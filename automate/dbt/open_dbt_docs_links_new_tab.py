aaf944b0d95a07cf28f3a55b547fb058967db77f
Bruno Antonellini <antonellinibruno@gmail.com>
2023-08-21 12:13:56 -0300

    Tune logic to avoid exiting the script with return codes

--- automate/dbt/open_dbt_docs_links_new_tab.py
+++ automate/dbt/open_dbt_docs_links_new_tab.py
@@ -13,26 +13,27 @@ if not dbt_docs_index_path.exists():
     console.print(
         "[red]:cross_mark:[/red] DBT docs not found, run [i]'dbt docs generate'[/i] in your project folder"
     )
-    exit(1)
-
-with open(dbt_docs_index_path, "r") as f:
-    html_content = f.read()
-
-new_tab_tag = "<base target='_blank'>"
-
-head_start = html_content.find("<head>") + len("<head>")
-head_end = html_content.find("</head>")
-
-if new_tab_tag not in html_content[head_start:head_end]:
-    modified_content = html_content[:head_end] + new_tab_tag + html_content[head_end:]
-
-    # Write the modified content back to the HTML file
-    with open(dbt_docs_index_path, "w") as html_file:
-        html_file.write(modified_content)
-    console.print(
-        "[green]:heavy_check_mark:[/green] dbt docs links will now open in a new tab"
-    )
 else:
-    console.print(
-        "[red]:cross_mark:[/red] dbt-docs links are already being opened in a new tab."
-    )
+    with open(dbt_docs_index_path, "r") as f:
+        html_content = f.read()
+
+    new_tab_tag = "<base target='_blank'>"
+
+    head_start = html_content.find("<head>") + len("<head>")
+    head_end = html_content.find("</head>")
+
+    if new_tab_tag not in html_content[head_start:head_end]:
+        modified_content = (
+            html_content[:head_end] + new_tab_tag + html_content[head_end:]
+        )
+
+        # Write the modified content back to the HTML file
+        with open(dbt_docs_index_path, "w") as html_file:
+            html_file.write(modified_content)
+        console.print(
+            "[green]:heavy_check_mark:[/green] dbt docs links will now open in a new tab"
+        )
+    else:
+        console.print(
+            "[red]:cross_mark:[/red] dbt docs links are already being opened in a new tab."
+        )
