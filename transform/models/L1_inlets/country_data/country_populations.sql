a22459754f981733d40dad6acbaf7c051f29f0c7
Noel Gomez <gomezn@convexa.ai>
2023-09-23 12:46:17 -0700

    model updates for now airbyte version

---
+++ transform/models/L1_inlets/country_data/country_populations.sql
@@ -0,0 +1,21 @@
+with raw_source as (
+
+    select *
+    from {{ source('RAW', 'COUNTRY_POPULATIONS') }}
+
+),
+
+final as (
+
+    select
+        year,
+        "COUNTRY NAME" as country_name,
+        value,
+        "COUNTRY CODE" as country_code
+
+    from raw_source
+
+)
+
+select * from final
+order by country_code
