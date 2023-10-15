3732def38776edcdc896c9f02a5f54af164005ae
Noel Gomez <gomezn@convexa.ai>
2023-10-15 08:56:39 -0700

    streamlit app loans data and dynamic table example

---
+++ transform/models/L3_coves/loan_analytics/loans_by_state.sql
@@ -0,0 +1,14 @@
+{{ config(
+    materialized = 'dynamic_table',
+    snowflake_warehouse = 'wh_transforming',
+    target_lag = '180 days',
+) }}
+
+select
+    addr_state as state,
+    count(*) as number_of_loans
+from
+    {{ source('LOANS', 'PERSONAL_LOANS') }}
+group by 1
+order by 2 desc
+limit 10
