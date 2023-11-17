ade2c1ea77c5b5217cb904d236f14137d2bdb7ee
Noel Gomez <gomezn@convexa.ai>
2023-11-16 17:33:46 -0800

    Fix key-based auth in streamlit

--- observe/streamlit/loans-example/loans.py
+++ observe/streamlit/loans-example/loans.py
@@ -10,6 +10,24 @@ import configparser, os #re, json
 from snowflake.snowpark import Session
 from snowflake.snowpark.context import get_active_session
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives import serialization
+def get_rsa_key(key_path):
+    with open(key_path, "rb") as key:
+        p_key= serialization.load_pem_private_key(
+            key.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+    pkb = p_key.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption())
+
+    return pkb
 
 def getSession():
     try:
@@ -19,10 +37,24 @@ def getSession():
         filename = os.path.join(os.path.expanduser('~'), ".snowsql/config")
         parser.read(filename)
         section = "connections"
+        auth = dict()
+
+        if 'password' in parser['connections']:
+            auth =  {"password": parser.get(section, "password")}
+            st.write('psw')
+        elif 'private_key_path' in parser['connections']:
+            key_path = parser.get(section, "private_key_path")
+            key = get_rsa_key(key_path)
+            auth =  {"private_key": key}
+            st.write('key found')
+        else:
+            st.write('Did not find password or key')
+
+
         pars = {
             "account": parser.get(section, "accountname"),
             "user": parser.get(section, "username"),
-            "password": parser.get(section, "password")
+            **auth
         }
 
         return Session.builder.configs(pars).create()
@@ -30,9 +62,10 @@ def getSession():
 def get_loan_data():
     conn = getSession()
 
+    conn.use_role('TRANSFORMER_DBT')
+
     data = conn.sql("select * from balboa.l3_loan_analytics.loans_by_state order by NUMBER_OF_LOANS desc") \
         .toPandas()
-    st.write('got data')
 
     return data
 
