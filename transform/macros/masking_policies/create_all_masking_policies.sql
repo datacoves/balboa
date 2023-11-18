3773b5300229a30d2042b87cf6163d62a37f63f8
Noel Gomez <noel_gomez@yahoo.com>
2023-11-18 14:44:51 -0800

    Update macros for dynamic data masking (#187)
    * Update how policies are created and updated
    * update masking policies macros
    
    Co-authored-by: Noel Gomez <gomezn@convexa.ai>

--- transform/macros/masking_policies/create_all_masking_policies.sql
+++ transform/macros/masking_policies/create_all_masking_policies.sql
@@ -1,6 +1,6 @@
-
 {# These macros create all the masking policies #}
 {#
+
     To run:
     dbt run-operation create_all_masking_policies -t prd_pii
 
@@ -8,25 +8,44 @@
 
 {% macro create_all_masking_policies() %}
 
-    {% set policies = ['string', 'float', 'date'] %}
+    {# Defines the policy types and the return value #}
+    {% set policies = [
+        ('string' , "********"),
+        ('float' , 0.00),
+        ('number' , 0),
+        ('date' , '0001-01-01 00:00:00.000' ),
+        ('timestamp_tz', '0001-01-01 00:00:00.000 +0000' ),
+        ('variant', ("masked","data"))
+    ] %}
+
     {% set masking_policy_db = var("common_masking_policy_db") %}
     {% set masking_policy_schema = var("common_masking_policy_schema") %}
 
-    {% for policy in policies %}
+    {% set existing_policies = ( masking_policy_list() ) %}
 
-        {% set call_masking_policy_macro = context["create_masking_policy_pii_full_field_" | string ~ policy | string] %}
-        {% set result = run_query(call_masking_policy_macro(masking_policy_db, masking_policy_schema)) %}
-        {{ print(result.columns[0].values()) }}
+    {% for data_type, mask_value in policies %}
+
+        {% set policy_name = ("masking_policy_pii_" ~ data_type | string) | upper %}
+        {% set fully_qualified_policy_name = masking_policy_db + '.' + masking_policy_schema + '.' + policy_name %}
+
+        {% if policy_name in existing_policies%}
+            {% set policy_currently_in_use = masking_policy_in_use(fully_qualified_policy_name) %}
+        {% else %}
+            {% set policy_currently_in_use = false %}
+        {% endif %}
+
+        {% set result = run_query(
+                create_update_masking_policy_pii(
+                        masking_policy_db,
+                        masking_policy_schema,
+                        data_type,
+                        mask_value,
+                        policy_currently_in_use
+                        )
+                    ) %}
 
     {% endfor %}
 
-    {% set sql %}
-
-        SHOW MASKING POLICIES in {{ masking_policy_db }}.{{ masking_policy_schema }};
-
-    {% endset %}
-
-    {% set result = run_query(sql) %}
-    {{ print(result.columns['name']) }}
-
+    {{ print("The following masking policies were created / updated: ")}}
+    {{ print( masking_policy_list() ) }}
 {% endmacro %}
