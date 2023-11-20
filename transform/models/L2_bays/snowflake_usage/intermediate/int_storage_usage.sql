5243552c7a3aa6ef68fcd6519cbf8adda84e6903
Noel Gomez <gomezn@convexa.ai>
2023-11-20 14:01:05 -0800

    delete most models to test masking

--- transform/models/L2_bays/snowflake_usage/intermediate/int_storage_usage.sql
+++
@@ -1,9 +0,0 @@
-select
-    date_trunc(month, usage_date) as usage_month,
-    avg(storage_bytes + stage_bytes + failsafe_bytes) / power(1024, 4) as total_billable_storage_tb,
-    avg(storage_bytes) / power(1024, 4) as storage_billable_storage_tb,
-    avg(stage_bytes) / power(1024, 4) as stage_billable_storage_tb,
-    avg(failsafe_bytes) / power(1024, 4) as failsafe_billable_storage_tb
-from {{ ref('storage_usage') }}
-group by date_trunc(month, usage_date)
-order by date_trunc(month, usage_date)
