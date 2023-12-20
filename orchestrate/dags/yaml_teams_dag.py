2952936ebee30f559dfe522253b10fa0741db167
Noel Gomez <gomezn@convexa.ai>
2023-12-19 16:18:56 -0800

    simplify sample dags

--- orchestrate/dags/yaml_teams_dag.py
+++ orchestrate/dags/yaml_teams_dag.py
@@ -1,13 +1,8 @@
 import datetime
 
-from airflow.decorators import dag, task_group
-from airflow.operators.bash import BashOperator
-from airflow.providers.airbyte.operators.airbyte import \
-    AirbyteTriggerSyncOperator
+from airflow.decorators import dag
 from callbacks.microsoft_teams import inform_failure, inform_success
-from fivetran_provider.operators.fivetran import FivetranOperator
-from fivetran_provider.sensors.fivetran import FivetranSensor
-from kubernetes.client import models as k8s
+from operators.datacoves.bash import DatacovesBashOperator
 
 
 def run_inform_success(context):
@@ -18,23 +13,6 @@ def run_inform_failure(context):
     inform_failure(context, connection_id="DATACOVES_MS_TEAMS", color="9900FF")
 
 
-TRANSFORM_CONFIG = {
-    "pod_override": k8s.V1Pod(
-        spec=k8s.V1PodSpec(
-            containers=[
-                k8s.V1Container(
-                    name="transform",
-                    image="datacoves/airflow-pandas:latest",
-                    resources=k8s.V1ResourceRequirements(
-                        requests={"memory": "8Gi", "cpu": "1000m"}
-                    ),
-                )
-            ]
-        )
-    ),
-}
-
-
 @dag(
     default_args={
         "start_date": datetime.datetime(2023, 1, 1, 0, 0),
@@ -42,7 +20,7 @@ TRANSFORM_CONFIG = {
         "email": "gomezn@example.com",
         "email_on_failure": True,
     },
-    description="Sample DAG with MS Teams notification, custom image, and resource requests",
+    description="Sample DAG with MS Teams notification",
     schedule_interval="0 0 1 */12 *",
     tags=["version_1", "ms_teams_notification", "blue_green"],
     catchup=False,
@@ -50,62 +28,14 @@ TRANSFORM_CONFIG = {
     on_failure_callback=run_inform_failure,
 )
 def yaml_teams_dag():
-    @task_group(group_id="extract_and_load_airbyte", tooltip="Airbyte Extract and Load")
-    def extract_and_load_airbyte():
-        personal_loans_datacoves_snowflake = AirbyteTriggerSyncOperator(
-            task_id="personal_loans_datacoves_snowflake",
-            connection_id="902432a8-cbed-4602-870f-33617fda6859",
-            airbyte_conn_id="airbyte_connection",
-        )
-        country_populations_datacoves_snowflake = AirbyteTriggerSyncOperator(
-            task_id="country_populations_datacoves_snowflake",
-            connection_id="ac02ea96-58a1-4061-be67-78900bb5aaf6",
-            airbyte_conn_id="airbyte_connection",
-        )
-
-    tg_extract_and_load_airbyte = extract_and_load_airbyte()
-
-    @task_group(
-        group_id="extract_and_load_fivetran", tooltip="Fivetran Extract and Load"
-    )
-    def extract_and_load_fivetran():
-        datacoves_snowflake_google_analytics_4_trigger = FivetranOperator(
-            task_id="datacoves_snowflake_google_analytics_4_trigger",
-            connector_id="speak_menial",
-            do_xcom_push=True,
-            fivetran_conn_id="fivetran_connection",
-        )
-        datacoves_snowflake_google_analytics_4_sensor = FivetranSensor(
-            task_id="datacoves_snowflake_google_analytics_4_sensor",
-            connector_id="speak_menial",
-            poke_interval=60,
-            fivetran_conn_id="fivetran_connection",
-        )
-        (
-            datacoves_snowflake_google_analytics_4_trigger
-            >> datacoves_snowflake_google_analytics_4_sensor
-        )
-
-    tg_extract_and_load_fivetran = extract_and_load_fivetran()
-    transform = BashOperator(
+    transform = DatacovesBashOperator(
         task_id="transform",
         bash_command="$DATACOVES__REPO_PATH/automate/blue_green_run.py -s 'tag:daily_run_airbyte+ tag:daily_run_fivetran+ -t prd'",
-        executor_config=TRANSFORM_CONFIG,
     )
-    transform.set_upstream([tg_extract_and_load_airbyte, tg_extract_and_load_fivetran])
-    marketing_automation = BashOperator(
-        task_id="marketing_automation",
-        bash_command="echo 'send data to marketing tool'",
-    )
-    marketing_automation.set_upstream([transform])
-    update_catalog = BashOperator(
-        task_id="update_catalog", bash_command="echo 'refresh data catalog'"
-    )
-    update_catalog.set_upstream([transform])
-    failing_task = BashOperator(
+    failing_task = DatacovesBashOperator(
         task_id="failing_task", bash_command="some_non_existant_command"
     )
-    failing_task.set_upstream([update_catalog])
+    failing_task.set_upstream([transform])
 
 
 dag = yaml_teams_dag()
