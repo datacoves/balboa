82bae5163e4f1fdfdcd963ddb21af40579deccbf
noel <noel_gomez@yahoo.com>
2023-12-29 19:39:07 +0000

    Deploying to dbt-docs from @ datacoves/balboa@768aa8c7679ebad3b82736301ca85289a9d2bcb5 🚀

--- run/balboa/models/L3_coves/loan_analytics/loans_by_state.sql
+++ run/balboa/models/L3_coves/loan_analytics/loans_by_state.sql
@@ -1,5 +1,25 @@
 
-        alter dynamic table "BALBOA_STAGING"."L3_LOAN_ANALYTICS"."LOANS_BY_STATE" set
+        
+    drop dynamic table if exists "BALBOA"."L3_LOAN_ANALYTICS"."LOANS_BY_STATE"
+;
+    create or replace dynamic table BALBOA.l3_loan_analytics.loans_by_state
+        target_lag = '190 days'
+        warehouse = wh_transforming
+        as (
             
-            warehouse = wh_transforming
+
+
+
+
+select
+    addr_state as state,
+    count(*) as number_of_loans
+from
+    RAW.LOANS.PERSONAL_LOANS
+group by 1
+order by 2 desc
+limit 10
+        )
+    ;
+    alter dynamic table BALBOA.l3_loan_analytics.loans_by_state refresh
     
