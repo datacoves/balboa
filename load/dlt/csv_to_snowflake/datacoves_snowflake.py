016d3e25804c88191b27c6c7574604fd332184a3
Noel Gomez <gomezn@convexa.ai>
2024-01-21 11:16:39 -0800

    debug

--- load/dlt/csv_to_snowflake/datacoves_snowflake.py
+++ load/dlt/csv_to_snowflake/datacoves_snowflake.py
@@ -1,10 +1,9 @@
 import os
 import dlt
 
-def set_config_value(key, env_var_prefix = 'DATACOVES__MAIN__', config_prefix = "destination.snowflake.credentials"):
+def set_config_value(key, config_key, env_var_prefix = 'DATACOVES__MAIN__'):
 
     env_var = env_var_prefix + key.upper()
-    config_key = "destination.snowflake.credentials." + key
 
     value = os.getenv(env_var, dlt.config[config_key])
 
@@ -13,9 +12,15 @@ def set_config_value(key, env_var_prefix = 'DATACOVES__MAIN__', config_prefix = 
 config_keys = ["account", "database", "warehouse", "role", "username", "password"]
 
 db_config = {}
-db_config['account'] = None
 for key in config_keys:
-    db_config[key] = set_config_value(key)
+    config_key = "destination.snowflake.credentials." + key
+
+    try:
+        dlt.config[config_key]
+    except dlt.common.configuration.exceptions.ConfigFieldMissingException:
+        dlt.config[config_key] = ''
+
+    db_config[key] = set_config_value(key, config_key)
 
 # This is needed because by default dlt calls the snowflake account host
 db_config['host'] = db_config['account']
