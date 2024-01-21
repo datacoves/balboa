8da86021b75f394f9b0823572bc18475813aaa4d
Noel Gomez <gomezn@convexa.ai>
2024-01-21 15:41:56 -0800

    debug

--- orchestrate/dags/daily_loan_run.py
+++ orchestrate/dags/daily_loan_run.py
@@ -8,13 +8,13 @@ from operators.datacoves.bash import DatacovesBashOperator
     default_args={"start_date": "2021-01"},
     description="Loan Run",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_8"],
+    tags=["version_9"],
     catchup=False,
 )
 def daily_loan_run():
     extract_and_load_dlt = DatacovesBashOperator(
         task_id="extract_and_load_dlt",
-        bash_command=" echo =========== && pwd && echo =========== && python csv_to_snowflake/load_csv_data.py && echo ===========",
+        bash_command=" echo =========== && pwd && echo =========== && ls -la && echo =========== && python csv_to_snowflake/load_csv_data.py && echo ===========",
     )
 
 
