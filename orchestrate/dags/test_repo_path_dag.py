231baf813fa960998707804a2b422a789c8d94d0
Bruno Antonellini <bruno@datacoves.com>
2024-01-24 04:53:24 -0800

    Temporary dag

--- orchestrate/dags/test_repo_path_dag.py
+++ orchestrate/dags/test_repo_path_dag.py
@@ -8,13 +8,13 @@ from operators.datacoves.bash import DatacovesBashOperator
     default_args={"start_date": "2021-01"},
     description="Echo DATACOVES__REPO_PATH",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_1"],
+    tags=["version_2"],
     catchup=False,
 )
 def test_repo_path():
     extract_and_load_dlt = DatacovesBashOperator(
         task_id="extract_and_load_dlt",
-        bash_command=" echo $DATACOVES__REPO_PATH",
+        bash_command=" ls -lha $DATACOVES__REPO_PATH",
     )
 
 
