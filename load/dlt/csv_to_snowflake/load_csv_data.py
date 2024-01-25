78fb1534e6723c7015f9b0c06c99de348ecf3d54
Noel Gomez <gomezn@convexa.ai>
2024-01-24 16:49:59 -0800

    debug

--- load/dlt/csv_to_snowflake/load_csv_data.py
+++ load/dlt/csv_to_snowflake/load_csv_data.py
@@ -1,36 +1,3 @@
-# import dlt
-# import pandas as pd
-# from datacoves_snowflake import db_config
-
-# if __name__ == "__main__":
-
-#     datacoves_snowflake = dlt.destinations.snowflake(
-#         db_config,
-#         destination_name="datacoves_snowflake"
-#     )
-
-#     us_population_csv = "https://raw.githubusercontent.com/dataprofessor/dashboard-v3/master/data/us-population-2010-2019.csv"
-#     df = pd.read_csv(us_population_csv)
-#     data = df.to_dict(orient="records")
-
-#     # dataset_name is the target schema name
-#     pipeline = dlt.pipeline(
-#         pipeline_name="from_csv",
-#         destination=datacoves_snowflake,
-#         dataset_name="us_population",
-#         progress="enlighten"
-#     )
-
-#     load_info = pipeline.run(
-#         data,
-#         table_name="us_population",
-#         write_disposition="replace"
-#     )
-
-#     print(load_info)
-
-
-
 import dlt
 import pandas as pd
 from datacoves_snowflake import db_config
@@ -41,7 +8,7 @@ from datacoves_snowflake import db_config
 def us_population():
     us_population_csv = "https://raw.githubusercontent.com/dataprofessor/dashboard-v3/master/data/us-population-2010-2019.csv"
     df = pd.read_csv(us_population_csv)
-    yield df.to_dict(orient="records") 
+    yield df
 
 # Source (corresponds to API or database)
 @dlt.source
