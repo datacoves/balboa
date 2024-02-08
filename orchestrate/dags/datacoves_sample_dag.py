81aa1fb14912e4807d9d71ebaa28ec73902a365b
Noel Gomez <noel_gomez@yahoo.com>
2024-02-07 16:22:03 -0800

    Update dags with changes to operators (#202)
    * update_dags
    * remove source
    * Adapt existent dags (#201)
    
    ---------
    
    Co-authored-by: Noel Gomez <gomezn@convexa.ai>

--- orchestrate/dags/datacoves_sample_dag.py
+++ orchestrate/dags/datacoves_sample_dag.py
@@ -30,19 +30,17 @@ def datacoves_sample_dag():
 
     # Calling dbt commands
     dbt_task = DatacovesDbtOperator(
-        task_id="run_dbt_task",
-        bash_command="dbt debug",
-        doc_md="""\
-            #### Task Documentation
-            This task leveraged the DatacovesDbtOperator
-        """,
+        task_id = "run_dbt_task",
+        bash_command = "dbt debug",
     )
 
     # This is calling an external Python file after activating the venv
     # use this instead of the Python Operator
     python_task = DatacovesBashOperator(
-        task_id="run_python_script",
-        bash_command="python orchestrate/python_scripts/sample_script.py",
+        task_id = "run_python_script",
+        # Virtual Environment is automatically activated
+        # activate_venv=True,
+        bash_command = "python orchestrate/python_scripts/sample_script.py"
     )
 
     # Define task dependencies
