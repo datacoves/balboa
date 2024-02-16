f6a6189cd197966bec22a43ddf30f3d6faac19ae
Alexander Butler <butler.alex2010@gmail.com>
2024-02-16 14:53:26 -0800

    chore: use env vars

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -21,12 +21,12 @@ def yaml_dbt_dag():
     run_dbt = DatacovesDbtOperator(
         task_id="run_dbt", bash_command=inspect.cleandoc("""
         dbt run -s personal_loans && \
-        curl -X POST http://core-dbt-api-svc.core.svc.cluster.local:80/api/internal/manifests \
-        -H "Authorization: Bearer UVJumixim6t2YDAlABFdVwIPob5pW432YHPGBtF9StrvIgiqqcoXlti-icX08lO5" \
+        curl -X POST $DATACOVES__UPLOAD_MANIFEST_URL \
+        -H "Authorization: Bearer $DATACOVES__UPLOAD_MANIFEST_TOKEN" \
         -F "environment_slug=$DATACOVES__ENVIRONMENT_SLUG" \
         -F "run_id=$AIRFLOW_CTX_DAG_RUN_ID" \
         -F "file=@target/manifest.json" \
-        --max-time 5
+        --max-time 10
         """)
     )
 
