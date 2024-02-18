c308531876d46b790dc2247360aad7b6f3bc5876
Noel Gomez <gomezn@convexa.ai>
2024-02-18 13:48:27 -0800

    rename dag and restore original

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -1,5 +1,4 @@
 import datetime
-import inspect
 
 from airflow.decorators import dag
 from operators.datacoves.dbt import DatacovesDbtOperator
@@ -14,22 +13,13 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     },
     description="Sample DAG for dbt build",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_3"],
+    tags=["version_2"],
     catchup=False,
 )
 def yaml_dbt_dag():
     run_dbt = DatacovesDbtOperator(
-        task_id="run_dbt", bash_command=inspect.cleandoc("""
-        dbt run -s personal_loans && \
-        curl -X POST $DATACOVES__UPLOAD_MANIFEST_URL \
-        -H "Authorization: Bearer $DATACOVES__UPLOAD_MANIFEST_TOKEN" \
-        -F "environment_slug=$DATACOVES__ENVIRONMENT_SLUG" \
-        -F "run_id=$AIRFLOW_CTX_DAG_RUN_ID" \
-        -F "file=@target/manifest.json" \
-        --max-time 120
-        """)
+        task_id="run_dbt", bash_command="dbt run -s personal_loans"
     )
-
 
 
 dag = yaml_dbt_dag()
---
+++ orchestrate/dags/yaml_dbt_dag_mesh.py
@@ -0,0 +1,38 @@
+import datetime
+import inspect
+
+from airflow.decorators import dag
+from operators.datacoves.dbt import DatacovesDbtOperator
+
+
+@dag(
+    default_args={
+        "start_date": datetime.datetime(2023, 1, 1, 0, 0),
+        "owner": "Noel Gomez",
+        "email": "gomezn@example.com",
+        "email_on_failure": True,
+    },
+    description="Sample DAG for dbt build",
+    schedule_interval="0 0 1 */12 *",
+    tags=["version_3"],
+    catchup=False,
+)
+def yaml_dbt_dag():
+    run_dbt = DatacovesDbtOperator(
+        task_id="run_dbt", bash_command=inspect.cleandoc("""
+        echo ###### &&
+        echo $DATACOVES__UPLOAD_MANIFEST_URL &&
+        echo ###### &&
+        dbt run -s personal_loans && \
+        curl -X POST $DATACOVES__UPLOAD_MANIFEST_URL \
+        -H "Authorization: Bearer $DATACOVES__UPLOAD_MANIFEST_TOKEN" \
+        -F "environment_slug=$DATACOVES__ENVIRONMENT_SLUG" \
+        -F "run_id=$AIRFLOW_CTX_DAG_RUN_ID" \
+        -F "file=@target/manifest.json" \
+        --max-time 120
+        """)
+    )
+
+
+
+dag = yaml_dbt_dag()
