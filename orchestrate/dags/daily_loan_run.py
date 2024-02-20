12d52f5ec10697e0d2c215224aaf9a1a5a06dc2f
Bruno Antonellini <bruno@datacoves.com>
2024-02-20 11:25:26 -0800

    Reimplement Fivetran on daily loan run

--- orchestrate/dags/daily_loan_run.py
+++ orchestrate/dags/daily_loan_run.py
@@ -2,6 +2,8 @@ import datetime
 
 from airflow.decorators import dag, task_group
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from fivetran_provider_async.operators import FivetranOperator
+from fivetran_provider_async.sensors import FivetranSensor
 from operators.datacoves.bash import DatacovesBashOperator
 from operators.datacoves.dbt import DatacovesDbtOperator
 
@@ -29,6 +31,29 @@ def daily_loan_run():
 
     tg_extract_and_load_airbyte = extract_and_load_airbyte()
 
+    @task_group(
+        group_id="extract_and_load_fivetran", tooltip="Fivetran Extract and Load"
+    )
+    def extract_and_load_fivetran():
+        datacoves_snowflake_google_analytics_4_trigger = FivetranOperator(
+            task_id="datacoves_snowflake_google_analytics_4_trigger",
+            connector_id="speak_menial",
+            do_xcom_push=True,
+            fivetran_conn_id="fivetran_connection",
+        )
+        datacoves_snowflake_google_analytics_4_sensor = FivetranSensor(
+            task_id="datacoves_snowflake_google_analytics_4_sensor",
+            connector_id="speak_menial",
+            poke_interval=60,
+            fivetran_conn_id="fivetran_connection",
+        )
+        (
+            datacoves_snowflake_google_analytics_4_trigger
+            >> datacoves_snowflake_google_analytics_4_sensor
+        )
+
+    tg_extract_and_load_fivetran = extract_and_load_fivetran()
+
     @task_group(group_id="extract_and_load_dlt", tooltip="dlt Extract and Load")
     def extract_and_load_dlt():
         load_us_population = DatacovesBashOperator(
@@ -46,6 +71,7 @@ def daily_loan_run():
         [
             tg_extract_and_load_airbyte,
             tg_extract_and_load_dlt,
+            tg_extract_and_load_fivetran,
         ]
     )
     marketing_automation = DatacovesBashOperator(
