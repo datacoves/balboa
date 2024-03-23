15e330ef5a828213220959f9ff309b24a7b869b7
Noel Gomez <gomezn@convexa.ai>
2024-03-23 07:57:20 -0700

    print just top level keys

--- training_and_demos/dbt-api/dbt_api.py
+++ training_and_demos/dbt-api/dbt_api.py
@@ -4,24 +4,26 @@ from importlib import reload
 from http import HTTPStatus
 from dotenv import load_dotenv
 
+import json
+from pygments import highlight, lexers, formatters
+
 def get_headers(token: str) -> dict:
     headers = {
         "Accept": "application/json",
         "Authorization": f"Bearer {token}"
     }
-    print("HEADERS:", headers)
+    # print("HEADERS:", headers)
     return headers
 
 def get_internal_endpoint(endpoint: str) -> str:
     url = f"{base_url_internal}/{endpoint}"
-    print("URL:", url)
+    print("URL:", url, "\n")
     return url
 
 
 def check_health():
     r = requests.get(url=get_internal_endpoint(endpoint="api/internal/healthcheck"))
     print_format(r)
-
 
 def gen_public_token():
     payload = {"token": {"account_id": 1}}
@@ -50,7 +52,6 @@ def regen_public_token():
         token = "PUBLIC TOKEN: Could not be created!!!"
     return token
 
-
 def upload_manifest():
     files = {'file': open('/config/workspace/transform/target/manifest.json','rb')}
 
@@ -66,8 +67,7 @@ def upload_manifest():
 
     print_format(r)
 
-
-def download_latest_manifest(trimmed=True):
+def download_latest_manifest(keys_only = True, trimmed = True):
     """Internal Manifest GET"""
     projects_slug = "balboa-analytics-datacoves"
     query_str = f"trimmed={str(trimmed).lower()}"
@@ -75,14 +75,31 @@ def download_latest_manifest(trimmed=True):
         url=get_internal_endpoint(endpoint=f"api/internal/projects/{projects_slug}/latest-manifest?{query_str}"),
         headers=get_headers(token=sa_user),
     )
-    print_format(r)
 
-def reload_lib(dbt):
-    reload(dbt)
-    print("Reloaded code!!")
+    print_format_json(r,  keys_only)
+
+def print_format_json(r, keys_only = True):
+    json_data = r.json()
+
+    if not isinstance(json_data, dict):
+        raise ValueError("JSON data is not a dictionary")
+
+    if keys_only:
+        top_level_keys = list(json_data.keys())#[:output_keys]
+        print(*top_level_keys, sep='\n')
+
+    else:
+        formatted_json = json.dumps(json_data, sort_keys=False, indent=2)
+        colorful_json = highlight(formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
+        print(colorful_json)
 
 def print_format(r):
     print("STATUS:", r.status_code, "| RESPONSE:", r.text)
+
+def reload_lib():
+    reload(dbt)
+    print("Reloaded code!!")
+
 
 load_dotenv()
 base_url_internal = os.getenv("DATACOVES__UPLOAD_MANIFEST_URL")
