dfe3e7df80d137824c31f25528e992cb9a79bf59
Bruno Antonellini <antonellinibruno@gmail.com>
2024-04-04 15:59:28 -0300

    2 concatenated tasks dag

--- orchestrate/dags/only_ls_with_datacoves_bash_operator.py
+++ orchestrate/dags/only_ls_with_datacoves_bash_operator.py
@@ -6,22 +6,27 @@ default_args = {
     "owner": "airflow",
     "email": "some_user@exanple.com",
     "email_on_failure": True,
-    "description": "Sample ls -alh dag",
+    "description": "Martin's dag",
 }
 
 
 @dag(
     default_args={"start_date": "2021-01"},
-    description="ls -alh",
+    description="Martin's dag",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_2"],
+    tags=["version_3"],
     catchup=False,
 )
-def ls_alh():
-    ls_alh = DatacovesBashOperator(
-        task_id="ls_alh",
-        bash_command=f"ls -alh",
+def martins_dag():
+    first_task = DatacovesBashOperator(
+        task_id="test_grid_first_Task",
+        bash_command=f"echo 'First Task'",
     )
+    second_task = DatacovesBashOperator(
+        task_id="test_grid_second_Task",
+        bash_command=f"echo 'Second Task'",
+    )
+    first_task >> second_task
 
 
-dag = ls_alh()
+dag = martins_dag()
