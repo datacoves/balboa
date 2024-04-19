150d6d6f7f5ad9b0c03a6e02de1400ef1fdc4a79
Noel Gomez <gomezn@convexa.ai>
2024-04-19 15:42:38 -0700

    Updated Ref macro to fix issue with dbt python models

--- transform/macros/tooling/ref.sql
+++ transform/macros/tooling/ref.sql
@@ -1,14 +1,33 @@
-{# 
-    Overrides default ref macro for staging databases, to not include database name in ref tag.
-    This creates relative links to allows swapping of those databases without breaking view references 
-    #}
+{#
+    Overrides default ref macro for staging databases, to not include database name.
+    This creates relative links to allows swapping of those databases without breaking view references
+#}
 
-{% macro ref(modelname) %}
-    {% set db_name = builtins.ref(modelname).database | lower %}}
-    {% if db_name.startswith('staging') or 
-        db_name.endswith('staging')  %}
-        {{ return(builtins.ref(modelname).include(database=false)) }}
-    {% else %}
-        {{ return(builtins.ref(modelname)) }}
+{% macro ref() %}
+
+    {% set version = kwargs.get('version') or kwargs.get('v') %}
+    {% set packagename = none %}
+    {%- if (varargs | length) == 1 -%}
+        {% set modelname = varargs[0] %}
+    {%- else -%}
+        {% set packagename = varargs[0] %}
+        {% set modelname = varargs[1] %}
     {% endif %}
+
+    {% set rel = None %}
+    {% if packagename is not none %}
+        {% set rel = builtins.ref(packagename, modelname, version=version) %}
+    {% else %}
+        {% set rel = builtins.ref(modelname, version=version) %}
+    {% endif %}
+
+
+    {% set db_name = rel.database | lower %}
+
+    {% if db_name.startswith('staging') or db_name.endswith('staging') %}
+        {% do return(rel.include(database=false)) %}
+    {% else %}
+        {% do return(rel) %}
+    {% endif %}
+
 {% endmacro %}
