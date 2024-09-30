a79c44020808f1f7b49d30dbab2fe81716787f31
Bruno Antonellini <antonellinibruno@gmail.com>
2024-09-30 10:04:13 -0300

    Data sync dag

--- orchestrate/dags/sync_airflow_db.py
+++ orchestrate/dags/sync_airflow_db.py
@@ -13,8 +13,9 @@ def sync_airflow_db():
     # service connection name default is 'airflow_db_load'.
     # Destination type default is 'snowflake' (and the only one supported for now)
     sync_data_script = DatacovesDataSyncOperatorSnowflake(
-        service_connection_name="airflow_db_load",  # this can be omitted or changed to another service connection name.
+        service_connection_name="main",  # this can be omitted or changed to another service connection name.
     )
+    sync_data_script
 
 
 dag = sync_airflow_db()
