67fa8739d71f4f0f9064ffc13da554502d53d2ca
Noel Gomez <NGomez3@ITS.JNJ.com>
2024-10-22 13:23:37 -0700

    check my_var val

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -20,8 +20,12 @@ print("######33333############")
     catchup=False,
 )
 def yaml_dbt_dag():
-    my_var = Variable.get("mayra_secret")
-    other_var = "TESTING"
+    my_var = Variable.get("mayra")
+    if my_var == 'noel':
+        other_var = "READIT"
+    else:
+        other_var = "NOSECRET"
+        
     run_dbt = DatacovesDbtOperator(
         task_id="run_dbt", bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo {other_var} && echo TTTTTTTTTTTTT && echo {my_var}"
     )
