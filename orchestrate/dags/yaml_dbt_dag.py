39070a1208df2ec9851f6e5054933c306829448e
Mayra Peña <MPena18@ITS.JNJ.com>
2024-10-28 14:45:00 -0700

    Testing Datacoves secrets backend with ngtest

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -18,16 +18,16 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     catchup=False,
 )
 def yaml_dbt_dag():
-    # my_var = Variable.get("ngtest")
-    # if my_var == 'noel':
-    #     other_var = "READIT"
-    # else:
-    #     other_var = "NOSECRET"
+    my_var = Variable.get("ngtest")
+    if my_var == 'noel':
+        other_var = "READIT"
+    else:
+        other_var = "NOSECRET"
 
     run_dbt = DatacovesDbtOperator(
         task_id="run_dbt",
-        bash_command="dbt run -s personal_loans",
-        # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
+        # bash_command="dbt run -s personal_loans",
+        bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
     )
 
 dag = yaml_dbt_dag()
