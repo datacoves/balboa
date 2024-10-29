d9aff0a8edfe13b628e48e934ecc0608d2006362
Mayra Peña <MPena18@ITS.JNJ.com>
2024-10-29 13:11:11 -0700

    ngtest

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -18,17 +18,19 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     catchup=False,
 )
 def yaml_dbt_dag():
-    # my_var = Variable.get("ngtest")
+    my_var = Variable.get("ngtest")
     # my_aws_var = Variable.get("aws_ngtest")
-    # if my_var == 'noel':
-    #     other_var = "READIT"
-    # else:
-    #     other_var = "NOSECRET"
+    if my_var == 'noel':
+        other_var = "READIT"
+    else:
+        other_var = "NOSECRET"
 
     run_dbt = DatacovesDbtOperator(
         task_id="run_dbt",
-        bash_command="dbt run -s personal_loans",
+        # bash_command="dbt run -s personal_loans",
+        bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
         # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var}"
+
     )
 
 dag = yaml_dbt_dag()
