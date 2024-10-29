1bb3b0bdc1b3193c7ffb989424c653019f8682d5
Mayra Peña <MPena18@ITS.JNJ.com>
2024-10-29 14:23:15 -0700

    Add aws variable

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -19,7 +19,7 @@ from operators.datacoves.dbt import DatacovesDbtOperator
 )
 def yaml_dbt_dag():
     my_var = Variable.get("ngtest")
-    # my_aws_var = Variable.get("aws_ngtest")
+    my_aws_var = Variable.get("aws_ngtest")
     if my_var == 'noel':
         other_var = "READIT"
     else:
@@ -28,8 +28,8 @@ def yaml_dbt_dag():
     run_dbt = DatacovesDbtOperator(
         task_id="run_dbt",
         # bash_command="dbt run -s personal_loans",
-        bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
-        # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var}"
+        # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
+        bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var}"
 
     )
 
