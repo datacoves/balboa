76059c2187240101475e9f65d82cd74a94a28a31
Mayra Peña <Mayra@datacoves.com>
2024-11-19 06:03:09 -0800

    Testing AWS

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -18,9 +18,9 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     catchup=False,
 )
 def yaml_dbt_dag():
-    # my_var = Variable.get("ngtest")
-    # my_aws_var = Variable.get("aws_ngtest")
-    # my_aws_secret = Variable.get('aws_ngtest_password')
+    my_var = Variable.get("ngtest")
+    my_aws_var = Variable.get("aws_ngtest")
+    my_aws_secret = Variable.get('aws_ngtest_password')
     # datacoves_secret = Variable.get('mayras_secret')
     if my_var == 'noel':
         other_var = "READIT"
@@ -31,6 +31,7 @@ def yaml_dbt_dag():
         task_id="run_dbt",
         bash_command="dbt run -s personal_loans",
         # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
+        bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var} && echo {my_aws_secret}"
         # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var} && echo {my_aws_secret} && echo {mayras_secret}"
 
     )
