de453014cbdab1036f13498a6246b04b81da3644
Noel Gomez <gomezn@convexa.ai>
2024-12-09 08:31:50 -0800

    rename data_testing folder to observe

--- observe/recce/run_recce.sh
+++ observe/recce/run_recce.sh
@@ -5,6 +5,6 @@ set -e
 
 # Configuration
 DBT_PROJECT_PATH=/config/workspace/transform
-RECCE_CONFIG_PATH=/config/workspace/data_testing/recce/recce.yml
+RECCE_CONFIG_PATH=/config/workspace/observe/recce/recce.yml
 
 recce run --target-base-path $DBT_PROJECT_PATH/logs --target-path $DBT_PROJECT_PATH/target --project-dir $DBT_PROJECT_PATH --config $RECCE_CONFIG_PATH
