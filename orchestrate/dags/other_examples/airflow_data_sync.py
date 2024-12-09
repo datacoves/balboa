cbd1ba151dd169c317e29fd49771bfffd28840f2
Noel Gomez <gomezn@convexa.ai>
2024-12-09 08:07:51 -0800

    rename dag and update tags

--- orchestrate/dags/other_examples/airflow_data_sync.py
+++ orchestrate/dags/other_examples/airflow_data_sync.py
@@ -2,30 +2,28 @@ import datetime
 
 from airflow.decorators import dag, task
 
-
 @dag(
     default_args={
         "start_date": datetime.datetime(2023, 1, 1, 0, 0),
-        "owner": "Bruno Antonellini",
-        "email": "bruno@datacoves.com",
+        "owner": "Bruno",
+        "email": "bruno@example.com",
         "email_on_failure": False,
     },
     description="Sample DAG for dbt build",
-    schedule_interval="0 0 1 */12 *",
-    tags=["version_1"],
+    schedule="0 0 1 */12 *",
+    tags=["extract_and_load"],
     catchup=False,
 )
-def decorated_snowflake_data_sync():
+def airflow_data_sync():
     @task.datacoves_airflow_db_sync(
         db_type="snowflake",
-        destination_schema="test_schema_nov_19",
+        destination_schema="airflow_dev",
         service_connection_name="snowflake_main",
         # additional_tables=["additional_table_1", "additional_table_2"],
     )
-    def data_sync_snowflake():
+    def sync_airflow_db():
         pass
 
-    data_sync_snowflake()
+    sync_airflow_db()
 
-
-dag = decorated_snowflake_data_sync()
+dag = airflow_data_sync()
