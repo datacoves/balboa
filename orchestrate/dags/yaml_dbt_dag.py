3c1a09a93ca7b3b25ed1a9f236135cbbe33bb7de
Mayra Peña <Mayra@datacoves.com>
2024-12-19 10:24:02 -0800

    Test a failing DAG

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -8,8 +8,8 @@ from operators.datacoves.dbt import DatacovesDbtOperator
 @dag(
     default_args={
         "start_date": datetime.datetime(2023, 1, 1, 0, 0),
-        "owner": "Noel Gomez",
-        "email": "gomezn@example.com",
+        "owner": "Mayra Pena",
+        "email": "mayra@datacoves.com",
         "email_on_failure": True,
     },
     description="Sample DAG for dbt build",
@@ -29,7 +29,7 @@ def yaml_dbt_dag():
 
     run_dbt = DatacovesDbtOperator(
         task_id="run_dbt",
-        bash_command="dbt run -s personal_loans",
+        bash_command="dbt run -s personal_loans_fail",
         # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
         # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var} && echo {my_aws_secret}"
         # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var} && echo {my_aws_secret} && echo {mayras_secret}"
