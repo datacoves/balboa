0e3345eab9cda0715d473f2a76b1a7f9b0a67633
Mayra Peña <Mayra@datacoves.com>
2024-12-20 14:09:33 -0800

    Testing default args

---
+++ orchestrate/dags/default_args_dag.py
@@ -0,0 +1,22 @@
+import datetime
+
+from airflow.decorators import dag
+from utils.default_args import default_args
+from operators.datacoves.dbt import DatacovesDbtOperator
+
+
+@dag(
+    default_args=default_args,
+    description="Daily dbt run",
+    schedule="0 12 * * *",
+    tags=["version_2"],
+    catchup=False,
+
+)
+def sample_dag():
+    run_dbt = DatacovesDbtOperator(
+        task_id="run_dbt", bash_command="dbt run -s country_codes"
+    )
+
+
+dag = sample_dag()
