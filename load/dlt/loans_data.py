54e9f7b9f7f4054ca914fcfa88e28e3913da3eaf
Noel Gomez <gomezn@convexa.ai>
2024-12-21 07:50:33 -0800

    update dlt version

--- load/dlt/loans_data.py
+++ load/dlt/loans_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S uv run --cache-dir /tmp/.uv_cache
 # /// script
 # dependencies = [
-#   "dlt[snowflake, parquet]==1.3.0",
+#   "dlt[snowflake, parquet]==1.5.0",
 #   "enlighten~=1.12.4",
 #   "psutil~=6.0.0",
 #   "pandas==2.2.2",
