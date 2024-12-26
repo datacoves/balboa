72be272592e327161825b034520781107815b58d
Noel Gomez <gomezn@convexa.ai>
2024-12-26 08:15:25 -0800

    test task group

--- orchestrate/dags/other_examples/datacoves_sample_dag.py
+++ orchestrate/dags/other_examples/datacoves_sample_dag.py
@@ -1,7 +1,7 @@
 """## Datacoves Operators Sample DAG
 This DAG is a sample using the Datacoves Airflow Operators"""
 
-from airflow.decorators import dag
+from airflow.decorators import dag, task_group
 from operators.datacoves.bash import DatacovesBashOperator
 from operators.datacoves.dbt import DatacovesDbtOperator
 from pendulum import datetime
@@ -27,25 +27,34 @@ DATACOVES_VIRTUAL_ENV = "/opt/datacoves/virtualenvs/main/bin/activate"
     schedule_interval="0 0 1 */12 *",
 )
 def datacoves_sample_dag():
-    var1= "YEEHAW"
-    # Calling dbt commands
-    dbt_task = DatacovesDbtOperator(
-        task_id = "run_dbt_task",
-        bash_command = "dbt debug",
-    )
+    @task_group(group_id="extract_and_load_dlt", tooltip="dlt Extract and Load")
+    def run_it():
+        dbt_task = DatacovesDbtOperator(
+            task_id = "run_dbt_task",
+            bash_command = "dbt debug",
+        )
 
-    # This is calling an external Python file after activating the venv
-    # use this instead of the Python Operator
-    python_task = DatacovesBashOperator(
-        task_id = "run_python_script",
-        # Virtual Environment is automatically activated
-        # activate_venv=True,
-        bash_command = "python orchestrate/python_scripts/sample_script.py",
-        env={'VAR1': var1}
-    )
+    run_it()
 
-    # Define task dependencies
-    python_task.set_upstream([dbt_task])
+    # var1= "YEEHAW"
+    # # Calling dbt commands
+    # dbt_task = DatacovesDbtOperator(
+    #     task_id = "run_dbt_task",
+    #     bash_command = "dbt debug",
+    # )
+
+    # # This is calling an external Python file after activating the venv
+    # # use this instead of the Python Operator
+    # python_task = DatacovesBashOperator(
+    #     task_id = "run_python_script",
+    #     # Virtual Environment is automatically activated
+    #     # activate_venv=True,
+    #     bash_command = "python orchestrate/python_scripts/sample_script.py",
+    #     env={'VAR1': var1}
+    # )
+
+    # # Define task dependencies
+    # python_task.set_upstream([dbt_task])
 
 # Invoke Dag
 dag = datacoves_sample_dag()
