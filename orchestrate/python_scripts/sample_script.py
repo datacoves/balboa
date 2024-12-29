43aeca8e42b9426e87c08ed76ff7978cf0aa3ca0
Noel Gomez <gomezn@convexa.ai>
2024-12-29 15:36:36 -0800

    fix typo

--- orchestrate/python_scripts/sample_script.py
+++ orchestrate/python_scripts/sample_script.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import os
 
-my_var = os.getenv("VAR1","NOT FOUND")
-
 def print_sample_dataframe():
 
     # Creating a simple DataFrame
@@ -15,14 +13,12 @@ def print_sample_dataframe():
     # Displaying the DataFrame
     print("DataFrame created using Pandas:")
     print(df)
-    print("my_var")
-    print(my_var)
 
 def print_vars():
     print("#########")
-    print(f"var1 is {os.getenv('VAR1')}")
-    print(f"var2 is {os.getenv('VAR2')}")
-    print(f"var3 is {os.getenv('VAR3')}")
+    print(f"var1 is { os.getenv('VAR1','NOT FOUND') }")
+    print(f"var2 is { os.getenv('VAR2') }")
+    print(f"var3 is { os.getenv('VAR3') }")
     print("#########")
 
 print_sample_dataframe()
