7305b177b8f45be7b4366fb4add0d3afce638692
Bruno Antonellini <antonellinibruno@gmail.com>
2025-01-03 11:32:44 -0300

    Remove env vars from pytest.ini, use conftest session fixture

--- orchestrate/test_dags/conftest.py
+++ orchestrate/test_dags/conftest.py
@@ -1,2 +1,12 @@
 # conftest.py
+import os
+
+import pytest
 from datacoves_airflow_provider.testing.custom_reporter import *
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env_vars():
+    os.environ["AIRFLOW_VAR_MY_VAR1"] = "my_var1"
+    os.environ["AIRFLOW_VAR_MY_VAR2"] = "my_var2"
+    os.environ["AIRFLOW_VAR_MY_VAR3"] = "my_var3"
