b3647daa50b3aa23f38942038eaf9e87e82fe8a6
Noel Gomez <gomezn@convexa.ai>
2025-01-30 15:19:02 -0800

    update basic yml dag

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -18,24 +18,9 @@ from operators.datacoves.dbt import DatacovesDbtOperator
     catchup=False,
 )
 def yaml_dbt_dag():
-    # my_var = Variable.get("ngtest")
-    # my_aws_var = Variable.get("aws_ngtest")
-    # my_aws_secret = Variable.get('aws_ngtest_password')
-    # datacoves_secret = Variable.get('mayras_secret')
-    # if my_var == 'noel':
-    #     other_var = "READIT"
-    # else:
-    #     other_var = "NOSECRET"
-
     run_dbt = DatacovesDbtOperator(
-        task_id="run_dbt",
-        bash_command="dbt run --warn-error -s personal_loans_fail",
-        # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var}"
-        # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var} && echo {my_aws_secret}"
-        # bash_command=f"echo TTTTTTTTTTTTT && dbt run -s personal_loans && echo && echo TTTTTTTTTTTTT && echo {my_var} && echo {my_aws_var} && echo {my_aws_secret} && echo {mayras_secret}"
-
+        task_id="run_dbt", bash_command="dbt run -s personal_loans"
     )
-
 
 
 dag = yaml_dbt_dag()
