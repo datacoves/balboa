2fbc59a059e7e7b7a35a9353df2a2fca16b5d38b
Bruno Antonellini <antonellinibruno@gmail.com>
2025-02-11 10:06:39 -0300

    Add S3 object storage dag

---
+++ orchestrate/dags/s3_upload.py
@@ -0,0 +1,21 @@
+from airflow.decorators import dag, task
+from airflow.io.path import ObjectStoragePath
+
+bucket = ObjectStoragePath("s3://datacoves-vs-code-images", conn_id="datacoves_s3")
+local_storage = ObjectStoragePath("/config/workspace")
+
+
+@dag(schedule=None, catchup=False, tags=["s3", "version_1"])
+def s3_upload_copy():
+    @task
+    def copy_files():
+        local_path = local_storage / ".yamllint"
+        destination_path = bucket / ".yamllint"
+        local_path.move(destination_path)
+        files = [f for f in destination_path.iterdir() if f.is_file()]
+        return files
+
+    copy_files()
+
+
+s3_upload_copy()
