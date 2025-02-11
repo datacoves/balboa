ef1f4070e146f2b99c30d102ff9740f686763abd
Bruno Antonellini <antonellinibruno@gmail.com>
2025-02-11 10:48:48 -0300

    add sample txt and change objectstorage path

--- orchestrate/dags/s3_upload.py
+++ orchestrate/dags/s3_upload.py
@@ -2,15 +2,15 @@ from airflow.decorators import dag, task
 from airflow.io.path import ObjectStoragePath
 
 bucket = ObjectStoragePath("s3://datacoves-vs-code-images", conn_id="datacoves_s3")
-local_storage = ObjectStoragePath("../../")
+local_storage = ObjectStoragePath("./files")
 
 
-@dag(schedule=None, catchup=False, tags=["s3", "version_2"])
+@dag(schedule=None, catchup=False, tags=["s3", "version_3"])
 def s3_upload_copy():
     @task
     def copy_files():
-        local_path = local_storage / ".yamllint"
-        destination_path = bucket / ".yamllint"
+        local_path = local_storage / "sample.txt"
+        destination_path = bucket / "sample.txt"
         local_path.move(destination_path)
         files = [f for f in destination_path.iterdir() if f.is_file()]
         return files
