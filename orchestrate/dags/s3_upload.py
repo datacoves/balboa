493b60feb4e1402f748decc87967f01d33d09e02
Bruno Antonellini <antonellinibruno@gmail.com>
2025-02-11 10:58:28 -0300

    Move sample to root

--- orchestrate/dags/s3_upload.py
+++ orchestrate/dags/s3_upload.py
@@ -2,10 +2,10 @@ from airflow.decorators import dag, task
 from airflow.io.path import ObjectStoragePath
 
 bucket = ObjectStoragePath("s3://datacoves-vs-code-images", conn_id="datacoves_s3")
-local_storage = ObjectStoragePath("./files")
+local_storage = ObjectStoragePath("/opt/airflow/dags/repo")
 
 
-@dag(schedule=None, catchup=False, tags=["s3", "version_3"])
+@dag(schedule=None, catchup=False, tags=["s3", "version_4"])
 def s3_upload_copy():
     @task
     def copy_files():
