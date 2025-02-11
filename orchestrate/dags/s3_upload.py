2f3c6d9fa4fa6a0efa21ecc8676cddff4208e933
Bruno Antonellini <antonellinibruno@gmail.com>
2025-02-11 10:42:16 -0300

    Change s3 upload

--- orchestrate/dags/s3_upload.py
+++ orchestrate/dags/s3_upload.py
@@ -2,10 +2,10 @@ from airflow.decorators import dag, task
 from airflow.io.path import ObjectStoragePath
 
 bucket = ObjectStoragePath("s3://datacoves-vs-code-images", conn_id="datacoves_s3")
-local_storage = ObjectStoragePath("/config/workspace")
+local_storage = ObjectStoragePath("../../")
 
 
-@dag(schedule=None, catchup=False, tags=["s3", "version_1"])
+@dag(schedule=None, catchup=False, tags=["s3", "version_2"])
 def s3_upload_copy():
     @task
     def copy_files():
