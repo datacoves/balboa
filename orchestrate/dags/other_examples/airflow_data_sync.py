e8cbb660bc8a3eb26265896cf3d8ddde62cc0dd7
Mayra Peña <MPena18@ITS.JNJ.com>
2025-02-17 14:41:43 -0800

    service connection test

--- orchestrate/dags/other_examples/airflow_data_sync.py
+++ orchestrate/dags/other_examples/airflow_data_sync.py
@@ -19,7 +19,7 @@ def airflow_data_sync():
     @task.datacoves_airflow_db_sync(
         db_type="snowflake",
         destination_schema="airflow_dev",
-        connection_id="main",
+        service_connection_name="main",
         # additional_tables=["additional_table_1", "additional_table_2"],
     )
     def sync_airflow_db():
