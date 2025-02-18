e7f1ce5223842de227af09e00fe3b79d6c783ba3
Bruno Antonellini <antonellinibruno@gmail.com>
2025-02-18 13:26:54 -0300

    New service connection name in Operator dag

--- orchestrate/dags/data_sync_operated_dag.py
+++ orchestrate/dags/data_sync_operated_dag.py
@@ -13,14 +13,14 @@ from pendulum import datetime
     default_args={"start_date": datetime(2024, 1, 1), "retries": 3},
     description="sync_data_script",
     schedule="0 0 1 */12 *",
-    tags=["extract_and_load"],
+    tags=["extract_and_load", "version_1"],
     catchup=False,
 )
 def data_sync_operator():
     # service connection name default is 'airflow_db_load'.
     # Destination type default is 'snowflake' (and the only one supported for now)
     sync_data_script = DatacovesDataSyncOperatorSnowflake(
-        service_connection_name="airflow_db_load",  # this can be omitted or changed to another service connection name.
+        service_connection_name="main",  # this can be omitted or changed to another service connection name.
     )
 
     sync_data_script
