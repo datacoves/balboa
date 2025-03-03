03e32dece72c48d0660047382e2db7c46be26ad3
Bruno Antonellini <antonellinibruno@gmail.com>
2025-03-03 09:04:48 -0300

    3 March dags

--- orchestrate/dags/basic_dag.py
+++ orchestrate/dags/basic_dag.py
@@ -3,16 +3,17 @@ from airflow.decorators import task
 from airflow.models.dag import DAG
 
 with DAG(
-    "dbt_decorator_rework",
+    "basic_dag",
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["dbt_decorator_rework", "version_1"],
+    tags=["basic_dag", "version_1"],
 ) as dag:
 
     @task.datacoves_dbt(
         connection_id="main",
-        upload_results=True,
+        download_files=False,
+        upload_results=False,
     )
     def dbt_ls():
         return "dbt ls"
---
+++ orchestrate/dags/reworked_dbt_decorator_upload.py
@@ -0,0 +1,34 @@
+import pendulum
+from airflow.decorators import task
+from airflow.models.dag import DAG
+
+with DAG(
+    "dbt_decorator_rework",
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["dbt_decorator_rework", "version_1"],
+) as dag:
+
+    @task.datacoves_dbt(
+        connection_id="main",
+        download_files=False,
+        upload_results=True,
+        # upload_tag="" TODO change to basic_dag id
+    )
+    def upload_artifacts():
+        return "dbt ls"
+
+    @task.datacoves_dbt(
+        connection_id="main",
+        download_files=True,
+        upload_results=False,
+        # upload_tag="" TODO change to basic_dag id
+    )
+    def download_artifacts(download_successful: bool = False):
+        if download_successful:
+            return "dbt ls"
+        else:
+            return "dbt debug"
+
+    upload_artifacts() >> download_artifacts()
