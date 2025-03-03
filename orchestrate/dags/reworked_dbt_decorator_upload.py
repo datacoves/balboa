a2fbd81fc0d64317fdf57f71a4db53365b68efab
Bruno Antonellini <antonellinibruno@gmail.com>
2025-03-03 10:20:56 -0300

    New upload_tag

--- orchestrate/dags/reworked_dbt_decorator_upload.py
+++ orchestrate/dags/reworked_dbt_decorator_upload.py
@@ -7,14 +7,14 @@ with DAG(
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["dbt_decorator_rework", "version_5"],
+    tags=["dbt_decorator_rework", "version_6"],
 ) as dag:
 
     @task.datacoves_dbt(
         connection_id="main",
         download_files=False,
         upload_results=True,
-        upload_tag="manual__2025-03-03T12:15:00.131343+00:00",  # TODO change to basic_dag id
+        upload_tag="manual__2025-03-03T13:19:50.146112+00:00",  # TODO change to basic_dag id
         upload_manifest=False,
     )
     def upload_artifacts():
@@ -24,7 +24,7 @@ with DAG(
         connection_id="main",
         download_files=True,
         upload_results=False,
-        upload_tag="manual__2025-03-03T12:15:00.131343+00:00",  # TODO change to basic_dag id
+        upload_tag="manual__2025-03-03T13:19:50.146112+00:00",  # TODO change to basic_dag id
     )
     def download_artifacts(download_successful: bool = False):
         if download_successful:
