0b1d042c4a21371ac28fd53be037cf7c2a0eedb3
Alejandro Morera <alejandro@datacoves.com>
2025-03-10 10:12:03 -0700

    Testing slack notifications

---
+++ orchestrate/dags/slack_dag.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+from airflow.providers.slack.notifications.slack import send_slack_notification
+
+@dag(
+    default_args={
+        "start_date": datetime.today() - timedelta(days=1),
+        "owner": "Alejandro",
+        "email": "alejandro@datacoves.com",
+        "email_on_failure": True,
+        "retries": 0,
+    },
+    description="Sample DAG with Slack notification, custom image, and resource requests",
+    schedule="0 0 1 */12 *",
+    tags=["version_1", "slack_notification"],
+    catchup=False,
+    on_success_callback=send_slack_notification(
+        text="The DAG {{ dag.dag_id }} succeeded", slack_webhook_conn_id="Datacoves_Slack"
+    ),
+    on_failure_callback=send_slack_notification(
+        text="The DAG {{ dag.dag_id }} failed", slack_webhook_conn_id="Datacoves_Slack"
+    ),
+)
+def slack_notification_dag():
+
+    @task.datacoves_dbt(connection_id="main")
+    def transform():
+        return "dbt debug"
+
+    transform()
+
+# Invoke DAG
+dag = slack_notification_dag()
