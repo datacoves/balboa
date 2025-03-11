10049b3b293f51c63633783ecd8f5af21b6210a7
Alejandro Morera <alejandro@datacoves.com>
2025-03-11 10:09:00 -0700

    Slack notification

--- orchestrate/dags/slack_dag.py
+++ orchestrate/dags/slack_dag.py
@@ -26,27 +26,27 @@ def my_python_task():
     },
     description="Sample DAG with Slack notification, custom image, and resource requests",
     schedule="0 0 1 */12 *",
-    tags=["version_7", "slack_notification"],
+    tags=["version_8", "slack_notification"],
     catchup=False,
 )
 def slack_notification_dag():
 
-    @task.datacoves_dbt(connection_id="main", on_success_callback=[run_inform_success],)
+    @task.datacoves_dbt(connection_id="main", on_success_callback=run_inform_success,)
     def transform():
         return "dbt debug"
 
     bash_task = BashOperator(
         task_id="bash_task",
         bash_command="echo 'Hola desde Bash'",
-        on_success_callback=[run_inform_success],
-        on_failure_callback=[run_inform_failure],
+        on_success_callback=run_inform_success,
+        on_failure_callback=run_inform_failure,
     )
 
     python_task = PythonOperator(
         task_id="python_task",
         python_callable=my_python_task,
-        on_success_callback=[run_inform_success],
-        on_failure_callback=[run_inform_failure],
+        on_success_callback=run_inform_success,
+        on_failure_callback=run_inform_failure,
     )
 
     transform() >> bash_task >> python_task
