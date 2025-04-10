c5f42713a224bf4a98bf08142cd6636d8abf7538
Noel Gomez <gomezn@datacoves.com>
2025-04-10 05:54:36 -0700

    fix governance errors

--- transform/models/L3_coves/earthquake_analytics/earthquakes_around_the_world.sql
+++ transform/models/L3_coves/earthquake_analytics/earthquakes_around_the_world.sql
@@ -37,7 +37,7 @@ final as (
         country_polygons.country_code_2 as country_code
     from earthquakes, country_polygons
     where country_polygons.geography is not NULL
-        and ST_CONTAINS(
+        and st_contains(
             country_polygons.geography,
             earthquakes.location_geo_point
         )
