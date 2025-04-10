6dd159a53ed0942e5063162c1bb277303dae6700
Noel Gomez <gomezn@datacoves.com>
2025-04-10 05:51:36 -0700

    update dataset dags

--- orchestrate/dags/data_aware/consumer.py
+++ orchestrate/dags/data_aware/consumer.py
@@ -1,27 +1,31 @@
-import datetime
+"""
+## Airflow Data Aware Consumer
+This DAG shows how to create a consumer DAG triggered by a dataset
+"""
 
 from airflow.decorators import dag, task
+
+from orchestrate.utils import datacoves_utils
 from orchestrate.utils.datasets import LAMBDA_UPDATED_DATASET, DAG_UPDATED_DATASET
 
-
 @dag(
-    default_args={
-        "start_date": datetime.datetime(2024, 1, 1, 0, 0),
-        "owner": "Noel Gomez",
-        "email": "gomezn@example.com",
-        "retries": 1
-    },
-    description="Sample Producer DAG",
-    schedule=(LAMBDA_UPDATED_DATASET | DAG_UPDATED_DATASET),
-    tags=["transform"],
-    catchup=False,
+    doc_md = __doc__,
+    catchup = False,
+
+    default_args = datacoves_utils.set_default_args(
+        owner = "Noel Gomez",
+        owner_email = "noel@example.com"
+    ),
+
+    description = "Sample Consumer DAG",
+    schedule = (LAMBDA_UPDATED_DATASET | DAG_UPDATED_DATASET),
+    tags = ["sample"],
 )
 def data_aware_consumer_dag():
     @task
-    def run_consumer():
+    def consume_a_dataset():
         print("I'm the consumer")
 
-    run_consumer()
+    consume_a_dataset()
 
-
-dag = data_aware_consumer_dag()
+data_aware_consumer_dag()
