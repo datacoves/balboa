69ad3f5cf327de849c684dc6408d5de6e1f5818a
Bruno Antonellini <antonellinibruno@gmail.com>
2025-04-10 16:54:49 -0300

    Final version of reworked decorator dag

--- orchestrate/dags/reworked_dbt_decorator_upload.py
+++ orchestrate/dags/reworked_dbt_decorator_upload.py
@@ -12,20 +12,14 @@ with DAG(
 
     @task.datacoves_dbt(
         connection_id="main",
-        # download_static_artifacts=False,
-        upload_static_artifacts=True, #semantic_manifest, partial_parse
-        # upload_tag="manual__2025-03-27T12:20:20.024277+00:00",  # TODO change to basic_dag id
-        # upload_manifest=False,
-        # upload_run_results=True,
-        # upload_sources_json=True,
+        upload_static_artifacts=True,
     )
     def dbt_source_freshness():
         return "dbt source freshness" # This should generate run_results and sources_json
-        # return "dbt ls"
 
     @task.datacoves_dbt(connection_id="main", upload_static_artifacts=False)
     def dbt_build():
-        return "dbt seed && dbt build -s stg_personal_loans+"
+        return "dbt seed && dbt build -s stg_personal_loans+" # Using 'dbt build' here to generate run results
 
     @task.datacoves_dbt(
         connection_id="main",
@@ -37,12 +31,6 @@ with DAG(
             return "dbt build -s result:error+ --state logs"
         else:
             return "dbt build -s stg_personal_loans+"
-                # dbt run -s 1+state:error+
-
-            """
-            dbt run -s state:error+
-            personal_loads- > model2
-            """
 
         
 
