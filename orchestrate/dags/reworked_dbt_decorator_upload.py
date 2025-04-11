16c8278850505f2d209822e69103bf71b70cf70d
Bruno Antonellini <antonellinibruno@gmail.com>
2025-04-11 14:41:13 -0300

    Version 17 of decorator rework

--- orchestrate/dags/reworked_dbt_decorator_upload.py
+++ orchestrate/dags/reworked_dbt_decorator_upload.py
@@ -7,7 +7,7 @@ with DAG(
     schedule=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=["dbt_decorator_rework", "version_16"],
+    tags=["dbt_decorator_rework", "version_17"],
 ) as dag:
 
     @task.datacoves_dbt(
@@ -17,16 +17,12 @@ with DAG(
     def dbt_source_freshness():
         return "dbt source freshness" # This should generate run_results and sources_json
 
-    @task.datacoves_dbt(connection_id="main", upload_static_artifacts=False)
-    def dbt_build():
-        return "dbt seed && dbt build -s stg_personal_loans+" # Using 'dbt build' here to generate run results
-
     @task.datacoves_dbt(
         connection_id="main",
         download_run_results=True,
         download_sources_json=True,
     )
-    def download_artifacts(expected_files: list = []):
+    def dbt_build(expected_files: list = []):
         if expected_files:
             return "dbt build -s result:error+ --state logs"
         else:
@@ -34,4 +30,4 @@ with DAG(
 
         
 
-    dbt_source_freshness() >> dbt_build() >> download_artifacts(expected_files=["run_results.json", "sources.json", "breaking_file.txt"])
+    dbt_source_freshness() >> dbt_build(expected_files=["run_results.json", "sources.json"])
