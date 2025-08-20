364065c79c5ea6142b2600370daf4fb0aa236cf8
Noel Gomez <gomezn@datacoves.com>
2025-08-20 11:36:45 -0700

    skip cloning if QA is recent

---
+++ transform/macros/tooling/blue-green/check_database_created_today.sql
@@ -0,0 +1,51 @@
+{# This macro checks if a database was created today #}
+{#
+    To run:
+    dbt run-operation check_database_created_today --args "{db_name: my_database}"
+
+    Returns: prints "true" if database was created today, "false" otherwise
+#}
+
+{%- macro check_database_created_today(db_name) -%}
+    {# Check if database exists and get its creation date #}
+    {% set check_db_sql %}
+        select
+            count(*) as db_count,
+            max(created) as creation_date
+        from information_schema.databases
+        where database_name = upper('{{ db_name }}')
+    {% endset %}
+
+    {% set db_result = run_query(check_db_sql) %}
+    {% set db_exists = db_result.columns[0].values()[0] > 0 %}
+
+    {% if not db_exists %}
+        {{ print("false") }}
+        {{ return("false") }}
+    {% endif %}
+
+    {% set creation_date = db_result.columns[1].values()[0] %}
+
+    {# Get today's date in UTC #}
+    {% set today_sql %}
+        select current_date() as today
+    {% endset %}
+
+    {% set today_result = run_query(today_sql) %}
+    {% set today = today_result.columns[0].values()[0] %}
+
+    {# Compare creation date with today #}
+    {% if creation_date %}
+        {% set creation_date_only = creation_date.date() %}
+        {% set created_today = creation_date_only == today %}
+
+        {% if created_today %}
+            {{ print("true") }}
+        {% else %}
+            {{ print("false") }}
+        {% endif %}
+    {% else %}
+        {{ print("false") }}
+    {% endif %}
+
+{%- endmacro -%}
