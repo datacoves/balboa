58eb5c61b7b65e6aa9828a0d40d90717fb4734ea
Bruno Antonellini <antonellinibruno@gmail.com>
2025-09-18 14:52:31 -0300

    Test new snowflake KP auth

--- orchestrate/dags/other_examples/airflow_data_sync.py
+++ orchestrate/dags/other_examples/airflow_data_sync.py
@@ -5,30 +5,35 @@ This DAG shows how to copy the Airflow Database to Snowflake
 
 from airflow.decorators import dag, task
 from orchestrate.utils import datacoves_utils
+from airflow.hooks.base import BaseHook
+
+kp_conn = BaseHook.get_connection("main_key_pair")
+
 
 @dag(
-    doc_md = __doc__,
-    catchup = False,
-
-    default_args = datacoves_utils.set_default_args(
-        owner = "Bruno",
-        owner_email = "bruno@example.com"
+    doc_md=__doc__,
+    catchup=False,
+    default_args=datacoves_utils.set_default_args(
+        owner="Bruno", owner_email="bruno@example.com"
     ),
-
-    description = "Sample DAG to synchronize the Airflow database",
-    schedule = datacoves_utils.set_schedule("0 0 1 */12 *"),
-    tags = ["extract_and_load"],
+    description="Sample DAG to synchronize the Airflow database",
+    schedule=datacoves_utils.set_schedule("0 0 1 */12 *"),
+    tags=["extract_and_load", "version_1"],
 )
 def airflow_data_sync():
     @task.datacoves_airflow_db_sync(
         db_type="snowflake",
         destination_schema="airflow_dev",
         connection_id="main_key_pair",
-        # additional_tables=["additional_table_1", "additional_table_2"],
+        env={
+            "DATA_SYNC_SNOWFLAKE_PRIVATE_KEY": kp_conn.extra_dejson.get("private_key"),
+        },
+        # additional_tables=["additional_table_1", "additional_table_2"]
     )
     def sync_airflow_db():
         pass
 
     sync_airflow_db()
+
 
 airflow_data_sync()
