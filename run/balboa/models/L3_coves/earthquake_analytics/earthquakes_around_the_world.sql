ced44919ff265e77bd467759fbe7ebd6a1f9d6e9
noel <noel_gomez@yahoo.com>
2025-10-27 23:23:35 +0000

    Deploying to dbt-docs from @ datacoves/balboa@c3e756c1a029f05704a1efa515033fa13a33f667 🚀

--- run/balboa/models/L3_coves/earthquake_analytics/earthquakes_around_the_world.sql
+++ run/balboa/models/L3_coves/earthquake_analytics/earthquakes_around_the_world.sql
@@ -1,56 +1,11 @@
-
+-- back compat for old kwarg name
   
+  begin;
     
 
-create or replace transient table BALBOA_STAGING.L3_EARTHQUAKE_ANALYTICS.earthquakes_around_the_world
-    
-
-    copy grants
-    as (
-
-with country_polygons as (
-
-    select
-        country_name,
-        country_code_2,
-        geography
-    from L1_COUNTRY_GEO.stg_country_polygons
-
-),
-
-earthquakes as (
-    select
-        location_geo_point,
-        sig,
-        case
-            when sig < 100 then 'Low'
-            when sig >= 100 and sig < 500 then 'Moderate'
-            else 'High'
-        end as sig_class,
-        earthquake_date
-    from L1_USGS__EARTHQUAKE_DATA.stg_earthquakes
-    
-
-),
-
-final as (
-
-    select
-        1 as ct,
-        earthquakes.*,
-        country_polygons.country_code_2 as country_code
-    from earthquakes, country_polygons
-    where country_polygons.geography is not null
-        and st_contains(
-            country_polygons.geography,
-            earthquakes.location_geo_point
-        )
-)
-
-
-select * from final
-    )
-;
-
-
-  
+        insert into BALBOA_STAGING.L3_EARTHQUAKE_ANALYTICS.earthquakes_around_the_world ("CT", "LOCATION_GEO_POINT", "SIG", "SIG_CLASS", "EARTHQUAKE_DATE", "COUNTRY_CODE")
+        (
+            select "CT", "LOCATION_GEO_POINT", "SIG", "SIG_CLASS", "EARTHQUAKE_DATE", "COUNTRY_CODE"
+            from BALBOA_STAGING.L3_EARTHQUAKE_ANALYTICS.earthquakes_around_the_world__dbt_tmp
+        );
+    commit;
