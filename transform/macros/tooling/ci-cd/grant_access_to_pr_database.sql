e080d0a8da9799085c122b8e793cf2268cd7f49a
Noel Gomez <gomezn@datacoves.com>
2025-10-28 15:18:36 -0700

    update roles and grant macros to make them more generic

--- transform/macros/tooling/ci-cd/grant_access_to_pr_database.sql
+++ transform/macros/tooling/ci-cd/grant_access_to_pr_database.sql
@@ -5,33 +5,16 @@
 #}
 
 {%- macro grant_access_to_pr_database() -%}
-    {% set db_role_name = 'z_db__balboa_tst' %}
     {% set db_name = target.database %}
+    {% set pr_db_usage_role = var("pr_db_usage_role") %}
 
     {% set apply_db_grants_sql %}
-        grant usage on database {{ db_name }} to role {{db_role_name}};
+        grant usage on database {{ db_name }} to role {{ pr_db_usage_role }};
+        grant select on all tables in database {{ db_name }} to role z_tables_views__select;
+        grant select on all views in database {{ db_name }} to role z_tables_views__select;
     {% endset %}
 
     {% do run_query(apply_db_grants_sql) %}
-
-    {% set schemas_list %}
-        select schema_name
-        from {{ db_name }}.information_schema.schemata
-        where schema_name not in ('INFORMATION_SCHEMA','PUBLIC','DBT_TEST__AUDIT')
-    {% endset %}
-
-    {% set schemas = run_query(schemas_list) %}
-    {% for schema in schemas %}
-
-        {% set apply_schema_grants_sql %}
-            grant usage on schema {{db_name}}.{{ schema[0] }} to z_schema__{{schema[0]}};
-            grant select on all tables in schema {{db_name}}.{{ schema[0] }} to role z_tables_views__select;
-            grant select on all views in schema {{db_name}}.{{ schema[0] }} to role z_tables_views__select;
-        {% endset %}
-
-        {% do run_query(apply_schema_grants_sql) %}
-        {{ log("Applied grants on Schema: " ~ db_name ~ '.' ~ schema[0], info=true) }}
-    {% endfor %}
 
     {{ log("Applied grants on Database: " ~ db_name, info=true) }}
 
