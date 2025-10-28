2679247d9d28b8f2f9f596ed9fa227959d3cb364
Noel Gomez <gomezn@datacoves.com>
2025-10-28 15:58:35 -0700

    grant usage on schemas in PR db

--- transform/macros/tooling/ci-cd/grant_access_to_pr_database.sql
+++ transform/macros/tooling/ci-cd/grant_access_to_pr_database.sql
@@ -7,6 +7,7 @@
 {%- macro grant_access_to_pr_database() -%}
     {% set db_name = target.database %}
     {% set pr_db_usage_role = var("pr_db_usage_role") %}
+    {% set schema_usage_role_prefix = var("schema_usage_role_prefix") %}
 
     {% set apply_db_grants_sql %}
         grant usage on database {{ db_name }} to role {{ pr_db_usage_role }};
@@ -15,6 +16,24 @@
     {% endset %}
 
     {% do run_query(apply_db_grants_sql) %}
+
+    {% set schemas_list %}
+        select schema_name
+        from {{ db_name }}.information_schema.schemata
+        where schema_name not in ('INFORMATION_SCHEMA','PUBLIC','DBT_TEST__AUDIT')
+    {% endset %}
+
+    {% set schemas = run_query(schemas_list) %}
+
+    {% for schema in schemas %}
+
+        {% set apply_schema_grants_sql %}
+            grant usage on schema {{ db_name }}.{{ schema[0] }} to {{ schema_usage_role_prefix }}{{ schema[0] }};
+        {% endset %}
+
+        {% do run_query(apply_schema_grants_sql) %}
+        {{ log("Applied grants on Schema: " ~ db_name ~ '.' ~ schema[0], info=true) }}
+    {% endfor %}
 
     {{ log("Applied grants on Database: " ~ db_name, info=true) }}
 
