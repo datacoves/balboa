defb6a7ae3bf1774a0c40b90b9ce7eee42491442
Noel Gomez <gomezn@datacoves.com>
2025-11-21 15:39:08 -0800

    Update retry dags

---
+++ orchestrate/dags/other_examples/retry_dbt_failures_cosmos.py
@@ -0,0 +1,53 @@
+"""
+## Retry dbt Example using Cosmos
+This DAG is an example of using Cosmos to run a dbt DAG
+"""
+from datetime import datetime
+from pathlib import Path
+import os
+
+# from airflow.decorators import dag, task
+from orchestrate.utils import datacoves_utils
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig, ExecutionMode, RenderConfig
+from cosmos.profiles import SnowflakePrivateKeyPemProfileMapping
+
+DBT_ROOT_PATH = Path(os.getenv("DATACOVES__DBT_HOME"))
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=SnowflakePrivateKeyPemProfileMapping(
+    conn_id = 'main_key_pair',
+),
+)
+
+VIRTUALENV = "/opt/datacoves/virtualenvs/main"
+
+retry_dbt_failure_cosmos = DbtDag(
+    project_config=ProjectConfig(
+        DBT_ROOT_PATH / '',
+    ),
+    execution_config=ExecutionConfig(
+        execution_mode=ExecutionMode.VIRTUALENV,
+        virtualenv_dir=VIRTUALENV,
+        dbt_executable_path=f"{VIRTUALENV}/bin/dbt",
+    ),
+    render_config=RenderConfig(
+        select=["stg_us_population+", "stg_personal_loans+"],
+    ),
+    profile_config=profile_config,
+    operator_args={
+        "install_deps": True,  # install any necessary dependencies before running any dbt command
+        "full_refresh": True,  # used only in dbt commands that support this flag
+    },
+
+    # normal dag parameters
+    schedule = datacoves_utils.set_schedule("0 0 1 */12 *"),
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="retry_dbt_failure_cosmos",
+    default_args={"retries": 2},
+    description="Sample DAG demonstrating how to run the dbt models that fail",
+    tags=["transform","retry"],
+ )
