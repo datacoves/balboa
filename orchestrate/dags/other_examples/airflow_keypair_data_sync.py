b6b01c7edeae880fdef9e9d5d4fdd5382d0bd302
Noel Gomez <noel_gomez@yahoo.com>
2026-01-03 10:22:00 -0800

    Feature/update airflow sync dags (#266)
    * update airflow sync dag names

--- orchestrate/dags/other_examples/airflow_keypair_data_sync.py
+++ orchestrate/dags/other_examples/airflow_keypair_data_sync.py
@@ -5,50 +5,9 @@ This DAG shows how to copy the Airflow Database to Snowflake
 
 from airflow.decorators import dag, task
 from orchestrate.utils import datacoves_utils
-from airflow.hooks.base import BaseHook
-from airflow.exceptions import AirflowNotFoundException
 from logging import Logger
-import os
 
 logger = Logger(__name__)
-
-DEFAULT_AIRFLOW_TABLES = [
-    "ab_permission",
-    "ab_role",
-    "ab_user",
-    "dag",
-    "dag_run",
-    "dag_tag",
-    "import_error",
-    "job",
-    "task_fail",
-    "task_instance",
-]
-
-
-def get_dbtcoves_datasync_env_vars(
-    connection_id: str, prefix: str = "DATA_SYNC_SNOWFLAKE_"
-):
-    conn = BaseHook.get_connection(connection_id)
-    conn_extras = conn.extra_dejson
-
-    env_vars = {
-        f"{prefix}USER": conn.login,
-        f"{prefix}WAREHOUSE": conn_extras.get("warehouse", ""),
-        f"{prefix}ACCOUNT": conn_extras.get("account", ""),
-        f"{prefix}ROLE": conn_extras.get("role", ""),
-        f"{prefix}DATABASE": conn_extras.get("database", ""),
-        "DATA_SYNC_SOURCE_CONNECTION_STRING": os.environ.get(
-            "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"
-        ),
-    }
-    if conn_extras.get("private_key_content"):
-        env_vars[f"{prefix}PRIVATE_KEY"] = conn_extras.get("private_key_content")
-    elif conn.password:
-        env_vars[f"{prefix}PASSWORD"] = conn.password
-
-    return env_vars
-
 
 @dag(
     doc_md=__doc__,
@@ -61,13 +20,15 @@ def get_dbtcoves_datasync_env_vars(
     tags=["extract_and_load", "version_7"],
 )
 def airflow_keypair_data_sync():
-    @task.datacoves_bash(
-        env=get_dbtcoves_datasync_env_vars("main_key_pair"), append_env=True
+    @task.datacoves_airflow_db_sync(
+        db_type="snowflake",
+        destination_schema="ng_airflow_dev",
+        connection_id="main_load_keypair",
+        # additional_tables=["additional_table_1", "additional_table_2"],
     )
-    def sync_airflow_db_v2():
-        return f"dbt-coves data-sync snowflake --source airflow_dev --tables '{','.join(DEFAULT_AIRFLOW_TABLES)}'"
+    def sync_airflow_db():
+        pass
 
-    sync_airflow_db_v2()
-
+    sync_airflow_db()
 
 airflow_keypair_data_sync()
