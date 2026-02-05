b4b830ac73ac790e9a3aab3429765c838f425b80
Noel Gomez <gomezn@datacoves.com>
2026-02-05 13:36:14 -0800

    Updates needed for Airflow 3

--- orchestrate/local/my_airflow/setup_airflow.sh
+++ orchestrate/local/my_airflow/setup_airflow.sh
@@ -5,4 +5,3 @@ uv venv
 uv pip install -r /config/workspace/orchestrate/local/my_airflow/requirements.txt
 uv pip install /config/workspace/orchestrate/local/my_airflow/providers/datacoves
 cp -r /config/workspace/orchestrate/local/my_airflow/plugins /config/airflow
-cp -r /config/workspace/orchestrate/local/my_airflow/airflow.cfg /config/airflow/
