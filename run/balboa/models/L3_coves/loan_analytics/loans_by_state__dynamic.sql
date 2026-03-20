518930688bd07479c4646b05412e3e4250249f57
noel <noel_gomez@yahoo.com>
2026-03-20 23:39:29 +0000

    Deploying to dbt-docs from @ datacoves/balboa@c06ff28242b218037dd87d1343b1e35deed2f06b 🚀

--- run/balboa/models/L3_coves/loan_analytics/loans_by_state__dynamic.sql
+++ run/balboa/models/L3_coves/loan_analytics/loans_by_state__dynamic.sql
@@ -1,5 +1,5 @@
 
-        alter dynamic table "BALBOA_STAGING"."L3_LOAN_ANALYTICS"."LOANS_BY_STATE__DYNAMIC" set
+        alter dynamic table "BALBOA"."L3_LOAN_ANALYTICS"."LOANS_BY_STATE__DYNAMIC" set
             
             warehouse = wh_transforming_dynamic_tables
     
