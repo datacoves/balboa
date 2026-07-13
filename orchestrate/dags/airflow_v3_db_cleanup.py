c5813ee04c4a28b2a00f05282042529db9b4809d
Datacoves Admin <hey@datacoves.com>
2026-07-13 13:46:56 -0700

    Airflow v3 db cleanup

--- orchestrate/dags/airflow_v3_db_cleanup.py
+++ orchestrate/dags/airflow_v3_db_cleanup.py
@@ -2,10 +2,10 @@ from pendulum import datetime
 from airflow.decorators import dag, task
 
 @dag(schedule=None, start_date=datetime(2026, 1, 1), catchup=False, tags=["maintenance"])
-def airflow_db_cleanup():
+def airflow_v3_db_cleanup():
     @task.datacoves_airflow_db_cleanup(retention_days=90, dry_run=True)
     def cleanup():
         pass
     cleanup()
 
-airflow_db_cleanup()
+airflow_v3_db_cleanup()
