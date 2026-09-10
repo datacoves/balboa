652d3e7cf2c8da0b76bfcc5c22024b659829afcd
noel <noel_gomez@yahoo.com>
2026-09-10 03:21:53 +0000

    Deploying to dbt-docs from @ datacoves/balboa@0f975708f020059a85a482ab3ad065fbe095ba28 🚀

--- run/balboa/models/L3_coves/loan_analytics/loans_by_state__dynamic.sql
+++ run/balboa/models/L3_coves/loan_analytics/loans_by_state__dynamic.sql
@@ -1,5 +1,45 @@
 
-        alter dynamic table "BALBOA"."L3_LOAN_ANALYTICS"."LOANS_BY_STATE__DYNAMIC" set
-            
-            warehouse = wh_transforming_dynamic_tables
+        
+
+    
+        create or replace transient dynamic table BALBOA.L3_LOAN_ANALYTICS.loans_by_state__dynamic
+    target_lag = '30 days'
+    warehouse = wh_transforming_dynamic_tables
+    
+
+    refresh_mode = AUTO
+
+    initialize = ON_CREATE
+
+    
+    scheduler = 'ENABLE'
+    
+    
+
+    
+
+    
+
+    copy grants
+    
+
+    as (
+        
+
+select
+    personal_loans.addr_state as state,
+    state_codes.state_name,
+    count(*) as number_of_loans
+
+from BALBOA.L1_LOANS.stg_personal_loans as personal_loans
+join BALBOA.SEEDS.state_codes as state_codes
+    on personal_loans.addr_state = state_codes.state_code
+group by state, state_name
+order by state_name desc
+limit 10
+    )
+
+    
+
+
     
